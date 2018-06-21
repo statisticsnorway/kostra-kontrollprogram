2fcb46a5c8050303ca55f5c5c374847046a4f2a1
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2018-06-21 10:15:48 +0200

    Slett sosintro, skjema 11B

---
+++ src/test/java/no/ssb/kostra/test/control/sensitiv/famvern/FamvernControlsTest.java
@@ -0,0 +1,7 @@
+package no.ssb.kostra.test.control.sensitiv.famvern;
+
+/**
+ * Created by ojj on 20.06.2018.
+ */
+public class FamvernControlsTest {
+}
---
+++ src/test/java/no/ssb/kostra/test/control/sensitiv/meklinger_55/MeklingerControlsTest.java
@@ -0,0 +1,7 @@
+package no.ssb.kostra.test.control.sensitiv.meklinger_55;
+
+/**
+ * Created by ojj on 20.06.2018.
+ */
+public class MeklingerControlsTest {
+}

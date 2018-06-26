15c79ccc4278673cca765048a42d5ea8094ada6d
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2018-06-26 10:06:37 +0200

    Oppdater kobling mellom fylke og kontor

--- src/main/java/no/ssb/kostra/control/sensitiv/famvern_53/ControlFylkeKontornummer.java
+++ src/main/java/no/ssb/kostra/control/sensitiv/famvern_53/ControlFylkeKontornummer.java
@@ -2,331 +2,82 @@ package no.ssb.kostra.control.sensitiv.famvern_53;
 
 /*
  */
- 
-import java.util.Vector;
+
 import no.ssb.kostra.control.Constants;
 
+import java.util.*;
+
 public final class ControlFylkeKontornummer extends no.ssb.kostra.control.Control
-    implements no.ssb.kostra.control.SingleRecordErrorReport
-{
-  private final String ERROR_TEXT = "K5: Manglende samsvar mellom fylkes- og kontornummer";
-  private Vector<Integer> lineNumbers = new Vector<Integer>();
+        implements no.ssb.kostra.control.SingleRecordErrorReport {
+    private final String ERROR_TEXT = "K5: Manglende samsvar mellom fylkes- og kontornummer";
+    private Vector<Integer> lineNumbers = new Vector<Integer>();
 
-  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet)
-  {
-    String fylkesNumber = RecordFields.getRegionNr(line);
-    String kontorNumber = RecordFields.getKontornummer(line);
+    public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
+        Map<String, List<String>> gyldigeFylkeKontor = new HashMap<>();
+        gyldigeFylkeKontor.put("01", Arrays.asList("016", "017"));
+        gyldigeFylkeKontor.put("02", Arrays.asList("021", "023", "024", "026"));
+        gyldigeFylkeKontor.put("03", Arrays.asList("037", "038", "039"));
+        gyldigeFylkeKontor.put("04", Arrays.asList("041", "043", "044", "045"));
+        gyldigeFylkeKontor.put("05", Arrays.asList("051", "052", "054"));
+        gyldigeFylkeKontor.put("06", Arrays.asList("061", "062", "064", "065"));
+        gyldigeFylkeKontor.put("07", Arrays.asList("071", "073"));
+        gyldigeFylkeKontor.put("08", Arrays.asList("081", "082"));
+        gyldigeFylkeKontor.put("09", Arrays.asList("091"));
+        gyldigeFylkeKontor.put("10", Arrays.asList("101"));
+        gyldigeFylkeKontor.put("11", Arrays.asList("111", "112"));
+        gyldigeFylkeKontor.put("12", Arrays.asList("121", "122", "123", "124", "125", "126", "127"));
+        gyldigeFylkeKontor.put("14", Arrays.asList("141", "142"));
+        gyldigeFylkeKontor.put("15", Arrays.asList("151", "152", "153"));
+        gyldigeFylkeKontor.put("16", Arrays.asList("162"));
+        gyldigeFylkeKontor.put("17", Arrays.asList("171", "172"));
+        gyldigeFylkeKontor.put("18", Arrays.asList("181", "182", "183", "184", "185"));
+        gyldigeFylkeKontor.put("19", Arrays.asList("191", "192", "193", "194"));
+        gyldigeFylkeKontor.put("20", Arrays.asList("201", "202", "203", "204", "205"));
 
-    {
-      String field1;
-      boolean isFilled = false;
-      boolean lineHasError = false;
+        String fylkesNumber = RecordFields.getRegionNr(line);
+        String kontorNumber = RecordFields.getKontornummer(line);
 
-      String field = fylkesNumber;
-      if (field.equalsIgnoreCase("01"))
-          {
-       	   field1 = kontorNumber;
-           field1 = field1.replace(' ','0');
-           if (
-            	field1.equalsIgnoreCase("016") ||
-            	field1.equalsIgnoreCase("017")
-              )
-            isFilled = true;      
-            else if (!field.equalsIgnoreCase("00"))
-            lineHasError = true;
-          }
-
-      else if (field.equalsIgnoreCase("02"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("021") ||
-           	field1.equalsIgnoreCase("023") ||
-           	field1.equalsIgnoreCase("024") ||
-           	field1.equalsIgnoreCase("026")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("03"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("037") ||
-           	field1.equalsIgnoreCase("038") ||
-           	field1.equalsIgnoreCase("039")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-      
-      else if (field.equalsIgnoreCase("04"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("041") ||
-           	field1.equalsIgnoreCase("043") ||
-           	field1.equalsIgnoreCase("044")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("05"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("051") ||
-           	field1.equalsIgnoreCase("052") ||
-           	field1.equalsIgnoreCase("054")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("06"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("061") ||
-           	field1.equalsIgnoreCase("062") ||
-           	field1.equalsIgnoreCase("064")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("07"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("071") ||
-           	field1.equalsIgnoreCase("073")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("08"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("081") ||
-           	field1.equalsIgnoreCase("082")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("09"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("091")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("10"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("101")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-      
-      else if (field.equalsIgnoreCase("11"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("111") ||
-           	field1.equalsIgnoreCase("112")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-      
-      else if (field.equalsIgnoreCase("12"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("121") ||
-           	field1.equalsIgnoreCase("122") ||
-           	field1.equalsIgnoreCase("123") ||
-           	field1.equalsIgnoreCase("124") ||
-           	field1.equalsIgnoreCase("125") ||
-           	field1.equalsIgnoreCase("126")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("14"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("141") ||
-           	field1.equalsIgnoreCase("142")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("15"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("151") ||
-           	field1.equalsIgnoreCase("152") ||
-           	field1.equalsIgnoreCase("153")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-      
-      else if (field.equalsIgnoreCase("16"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("162")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("17"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("171") ||
-           	field1.equalsIgnoreCase("172")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("18"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("181") ||
-           	field1.equalsIgnoreCase("182") ||
-           	field1.equalsIgnoreCase("183") ||
-           	field1.equalsIgnoreCase("184") ||
-           	field1.equalsIgnoreCase("185")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("19"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("191") ||
-           	field1.equalsIgnoreCase("192") ||
-           	field1.equalsIgnoreCase("193")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-
-      else if (field.equalsIgnoreCase("20"))
-      {
-      	  field1 = kontorNumber;
-          field1 = field1.replace(' ','0');
-          if (
-           	field1.equalsIgnoreCase("201") ||
-           	field1.equalsIgnoreCase("202") ||
-           	field1.equalsIgnoreCase("203") ||
-           	field1.equalsIgnoreCase("204")
-             )
-           isFilled = true;      
-           else if (!field.equalsIgnoreCase("00"))
-           lineHasError = true;
-      }
-      
-      
-          if (lineHasError || !isFilled)
-          {
-        	lineNumbers.add (new Integer (lineNumber));
-          }
-          return lineHasError || !isFilled;
-    }
-  }
-
-  public String getErrorReport (int totalLineNumber)
-  {
-    String errorReport = ERROR_TEXT + ":" + lf + lf;
-    if (foundError())
-    {
-      int numOfRecords = lineNumbers.size();
-      errorReport += "\tFeil: i " + numOfRecords + 
-      " record" + (numOfRecords == 1 ? "" : "s") + 
-      " fylkesnummer og kontornummer stemmer ikke overens."; 
-      if (numOfRecords <= 10)
-      {
-        errorReport += lf + "\t\t(Gjelder record nr.";
-        for (int i=0; i<numOfRecords; i++)
-        {
-          errorReport += " " + lineNumbers.elementAt(i);
+        String field = fylkesNumber;
+        if (gyldigeFylkeKontor.containsKey(fylkesNumber)){
+            List<String> gyldigeKontor = gyldigeFylkeKontor.get(fylkesNumber);
+            if (gyldigeKontor.contains(kontorNumber)){
+                return false;
+            }
         }
-        errorReport += ").";
-      }
+
+        lineNumbers.add(new Integer(lineNumber));
+
+        return true;
     }
-    errorReport += lf + lf;
-    return errorReport;
-  }
-  
-  public boolean foundError()
-  {
-    return lineNumbers.size() > 0;
-  }  
 
-  public String getErrorText()
-  {
-    return  ERROR_TEXT;
-  }
+    public String getErrorReport(int totalLineNumber) {
+        String errorReport = ERROR_TEXT + ":" + lf + lf;
+        if (foundError()) {
+            int numOfRecords = lineNumbers.size();
+            errorReport += "\tFeil: i " + numOfRecords +
+                    " record" + (numOfRecords == 1 ? "" : "s") +
+                    " fylkesnummer og kontornummer stemmer ikke overens.";
+            if (numOfRecords <= 10) {
+                errorReport += lf + "\t\t(Gjelder record nr.";
+                for (int i = 0; i < numOfRecords; i++) {
+                    errorReport += " " + lineNumbers.elementAt(i);
+                }
+                errorReport += ").";
+            }
+        }
+        errorReport += lf + lf;
+        return errorReport;
+    }
 
-  public int getErrorType() {
-    return Constants.NORMAL_ERROR;
-  }
+    public boolean foundError() {
+        return lineNumbers.size() > 0;
+    }
+
+    public String getErrorText() {
+        return ERROR_TEXT;
+    }
+
+    public int getErrorType() {
+        return Constants.NORMAL_ERROR;
+    }
 }

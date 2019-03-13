2e3ba1303aae66b1f43602b40cc365e8bd5de03d
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2019-03-13 12:04:01 +0100

    Bugfix for KOSTRAIT-107 og KOSTRAIT-108

--- src/main/java/no/ssb/kostra/control/regnskap/regn0F/ControlOrgNummer.java
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0F/ControlOrgNummer.java
@@ -1,56 +1,49 @@
 package no.ssb.kostra.control.regnskap.regn0F;
 
-import java.util.Vector;
 import no.ssb.kostra.control.Constants;
-import no.ssb.kostra.utils.OrgNrForKirkeKOSTRA;
 
-final class ControlOrgNummer extends no.ssb.kostra.control.Control
-{
-  private Vector<Integer> recordNumbers = new Vector<Integer>();
+import java.util.Vector;
 
-  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet)
-  {
-    String orgnr = RecordFields.getOrgNummer (line);
-    OrgNrForKirkeKOSTRA orgnr_checker = new OrgNrForKirkeKOSTRA();
+final class ControlOrgNummer extends no.ssb.kostra.control.Control {
+    private Vector<Integer> recordNumbers = new Vector<Integer>();
 
-    boolean line_has_error = ! orgnr_checker.isValidOrgNr(orgnr);
+    public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
+        String orgNum = RecordFields.getOrgNummer(line).trim();
 
-    if (line_has_error)
-    {
-      recordNumbers.add (new Integer (lineNumber));
-    }
-    return line_has_error;
-  }
+        boolean lineHasError = (!orgNum.equalsIgnoreCase(statistiskEnhet));
 
-  public String getErrorReport (int totalLineNumber)
-  {
-    String errorReport = "Kontroll 4, organisasjonsnummer:" + lf + lf;
-    int numOfRecords = recordNumbers.size();
-    if (numOfRecords > 0)
-    {
-      errorReport += "\t Feil: Ukjent organisasjonsnummer. " + lf + 
-          "\t\t" + numOfRecords + " av " + totalLineNumber + 
-          " records har et ukjent organisasjonsnummer.";
-      if (numOfRecords <= 10)
-      {
-        errorReport += "(Gjelder følgende records:";
-        for (int i=0; i<numOfRecords; i++)
-        {
-          errorReport += " " + recordNumbers.elementAt(i);
+        if (lineHasError) {
+            recordNumbers.add(new Integer(lineNumber));
         }
-        errorReport += ")";
-      }
-      errorReport += lf + lf;
+
+        return lineHasError;
+
     }
-    return errorReport;
-  }
 
-  public boolean foundError()
-  {
-    return recordNumbers.size() > 0;
-  }  
+    public String getErrorReport(int totalLineNumber) {
+        String errorReport = "Kontroll 4, organisasjonsnummer:" + lf + lf;
+        int numOfRecords = recordNumbers.size();
+        if (numOfRecords > 0) {
+            errorReport += "\t Feil: Ukjent organisasjonsnummer. " + lf +
+                    "\t\t" + numOfRecords + " av " + totalLineNumber +
+                    " records har et ukjent organisasjonsnummer.";
+            if (numOfRecords <= 10) {
+                errorReport += "(Gjelder følgende records:";
+                for (int i = 0; i < numOfRecords; i++) {
+                    errorReport += " " + recordNumbers.elementAt(i);
+                }
+                errorReport += ")";
+            }
+            errorReport += lf + lf;
+        }
+        return errorReport;
+    }
 
-  public int getErrorType() {
-    return Constants.CRITICAL_ERROR;
-  }
+    public boolean foundError() {
+        return recordNumbers.size() > 0;
+    }
+
+    public int getErrorType() {
+        return Constants.CRITICAL_ERROR;
+    }
 }
--- src/main/java/no/ssb/kostra/control/regnskap/regn0G/Main.java
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0G/Main.java
@@ -1,140 +1,133 @@
 package no.ssb.kostra.control.regnskap.regn0G;
 
-import java.io.*;
-import java.util.*;
+import no.ssb.kostra.control.Constants;
+import no.ssb.kostra.control.Control;
+import no.ssb.kostra.control.ControlRecord;
 import no.ssb.kostra.utils.Regioner;
-import no.ssb.kostra.control.*;
 
-public final class Main 
-{
-  private String regionNumber;
-  private File sourceFile;
-  private File reportFile;
-  private String[] regnskap;
-  private Control[] controls;
-  private final String lf = Constants.lineSeparator;
-  private final String VERSION = "0G." + Constants.kostraYear + ".01";
-  
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Calendar;
 
-  public Main 
-      (String regionNumber, File sourceFile, File reportFile, String[] regnskap)
-  {
-    this.regionNumber = regionNumber;
-    this.sourceFile = sourceFile;
-    this.reportFile = reportFile;
-    this.regnskap = regnskap;
-    initControls();
-  }
+public final class Main {
+    private final String lf = Constants.lineSeparator;
+    private final String VERSION = "0G." + Constants.kostraYear + ".01";
+    private String regionNumber;
+    private File sourceFile;
+    private File reportFile;
+    private String[] regnskap;
+    private Control[] controls;
+    private String statistiskEnhet;
 
-  public int start()
-  {
-    int lineNumber = 0;
-    int error_type = Constants.NO_ERROR;
 
-    for (int j=0; j<regnskap.length; j++) {
-    
-      if (! regnskap[j].equalsIgnoreCase("")) {
-
-        lineNumber += 1;
-
-        //Sjekker recordlengde forst, fordi feil reccordlengde 
-        //vil kunne odelegge mange andre kontroller 
-        //(StringIndexOutOfBoundsException etc.)
-        if (! controls[0].doControl(regnskap[j], lineNumber, regionNumber, ""))
-        {
-          for (int i=1; i<controls.length; i++)
-          {
-            controls[i].doControl(regnskap[j], lineNumber, regionNumber, "");
-          }
-        }
-      }
+    public Main
+            (String regionNumber, File sourceFile, File reportFile, String[] regnskap, String statistiskEnhet) {
+        this.regionNumber = regionNumber;
+        this.sourceFile = sourceFile;
+        this.reportFile = reportFile;
+        this.regnskap = regnskap;
+        this.statistiskEnhet = (statistiskEnhet != null) ? statistiskEnhet : "0";
+        initControls();
     }
 
-    try
-    {
-      BufferedWriter fileWriter = null;
-      if (reportFile != null)
-        fileWriter = new BufferedWriter (new FileWriter (reportFile));
+    public int start() {
+        int lineNumber = 0;
+        int error_type = Constants.NO_ERROR;
 
-      String report =  "-------------------------------------------------" + lf;    
-             report += lf;
-             report += " Feilmeldingsrapport for " + regionNumber + " " + Regioner.getRegionName(regionNumber) + lf;
-             report += lf;
-             report += "-------------------------------------------------" + lf + lf;
+        for (int j = 0; j < regnskap.length; j++) {
 
-      report += "Kontrollprogramversjon: " + VERSION + lf;
-      report += "Rapport generert: " + Calendar.getInstance().getTime() + lf;
-      report += "Kontrollert fil: " + (sourceFile != null ? sourceFile.getAbsolutePath() : "") + lf;
-      report += "Type regnskap: 0G - Balanseregnskap kirkelige fellesråd" + lf + lf;
+            if (!regnskap[j].equalsIgnoreCase("")) {
 
-      int numOfErrors = 0;
-      int control_error_type;
-      String buffer = "";
-      
-      for (int i=0; i<controls.length; i++)
-      {
-        if (controls[i].foundError())
-        {
-          control_error_type = controls[i].getErrorType();
-          if (control_error_type > error_type)
-            error_type = control_error_type;
+                lineNumber += 1;
 
-          if (control_error_type == Constants.CRITICAL_ERROR)
-             buffer +=Constants.CRITICAL_ERROR_TEXT_MSG + lf;
-          
-          buffer += controls[i].getErrorReport (lineNumber);
-          numOfErrors += 1;
+                //Sjekker recordlengde forst, fordi feil reccordlengde
+                //vil kunne odelegge mange andre kontroller
+                //(StringIndexOutOfBoundsException etc.)
+                if (!controls[0].doControl(regnskap[j], lineNumber, regionNumber, statistiskEnhet)) {
+                    for (int i = 1; i < controls.length; i++) {
+                        controls[i].doControl(regnskap[j], lineNumber, regionNumber, statistiskEnhet);
+                    }
+                }
+            }
         }
-      }
 
-      if (numOfErrors > 0)
-      {
-        report += "Følgende " + (numOfErrors == 1 ? "kontroll" : (numOfErrors + " kontroller")) + " har funnet feil eller advarsler:" +lf + lf;
-        report += buffer;  
-      }
-      else
-      {
-        if (lineNumber == 0) {
-          report += "Finner ingen data!";
-          error_type = Constants.CRITICAL_ERROR;
-        } else {          
-          report += "Ingen feil funnet!";
+        try {
+            BufferedWriter fileWriter = null;
+            if (reportFile != null)
+                fileWriter = new BufferedWriter(new FileWriter(reportFile));
+
+            String report = "-------------------------------------------------" + lf;
+            report += lf;
+            report += " Feilmeldingsrapport for " + regionNumber + " " + Regioner.getRegionName(regionNumber) + lf;
+            report += lf;
+            report += "-------------------------------------------------" + lf + lf;
+
+            report += "Kontrollprogramversjon: " + VERSION + lf;
+            report += "Rapport generert: " + Calendar.getInstance().getTime() + lf;
+            report += "Kontrollert fil: " + (sourceFile != null ? sourceFile.getAbsolutePath() : "") + lf;
+            report += "Type regnskap: 0G - Balanseregnskap kirkelige fellesråd" + lf + lf;
+
+            int numOfErrors = 0;
+            int control_error_type;
+            String buffer = "";
+
+            for (int i = 0; i < controls.length; i++) {
+                if (controls[i].foundError()) {
+                    control_error_type = controls[i].getErrorType();
+                    if (control_error_type > error_type)
+                        error_type = control_error_type;
+
+                    if (control_error_type == Constants.CRITICAL_ERROR)
+                        buffer += Constants.CRITICAL_ERROR_TEXT_MSG + lf;
+
+                    buffer += controls[i].getErrorReport(lineNumber);
+                    numOfErrors += 1;
+                }
+            }
+
+            if (numOfErrors > 0) {
+                report += "Følgende " + (numOfErrors == 1 ? "kontroll" : (numOfErrors + " kontroller")) + " har funnet feil eller advarsler:" + lf + lf;
+                report += buffer;
+            } else {
+                if (lineNumber == 0) {
+                    report += "Finner ingen data!";
+                    error_type = Constants.CRITICAL_ERROR;
+                } else {
+                    report += "Ingen feil funnet!";
+                }
+            }
+
+            if (fileWriter != null) {
+                fileWriter.write(report);
+                fileWriter.close();
+            } else {
+                System.out.println(report);
+            }
+        } catch (Exception e) {
+            System.out.println("Can't write report file: ");
+            System.out.println(e.toString());
+            ;
+            System.exit(Constants.IO_ERROR);
         }
-      }
-      
-      if (fileWriter != null)
-      {  
-        fileWriter.write (report);
-        fileWriter.close();
-      } else {
-        System.out.println(report);
-      }     
+
+        return error_type;
     }
-    catch (Exception e)
-    {
-      System.out.println("Can't write report file: ");
-      System.out.println(e.toString());;
-      System.exit (Constants.IO_ERROR);
-    }    
 
-    return error_type;
-  }
-
-  private void initControls()
-  {
-    controls = new Control[13];
-    controls[0] = new ControlRecordlengde();
-    controls[1] = new ControlAargang();
-    controls[2] = new ControlKommunenummer();
-    controls[3] = new ControlOrgNummer();
-    controls[4] = new ControlKontoklasse();
-    controls[5] = new ControlKapitler();
-    controls[6] = new ControlSummering();
-    controls[7] = new ControlDubletter();
-    controls[8] = new ControlAntallSifferBelop();
-    controls[9] = new Control10();
-    controls[10] = new ControlRecord();
-    controls[11] = new no.ssb.kostra.control.regnskap.ControlOrgNr();
-    controls[12] = new ControlNumericalFields();
-  }
+    private void initControls() {
+        controls = new Control[13];
+        controls[0] = new ControlRecordlengde();
+        controls[1] = new ControlAargang();
+        controls[2] = new ControlKommunenummer();
+        controls[3] = new ControlOrgNummer();
+        controls[4] = new ControlKontoklasse();
+        controls[5] = new ControlKapitler();
+        controls[6] = new ControlSummering();
+        controls[7] = new ControlDubletter();
+        controls[8] = new ControlAntallSifferBelop();
+        controls[9] = new Control10();
+        controls[10] = new ControlRecord();
+        controls[11] = new no.ssb.kostra.control.regnskap.ControlOrgNr();
+        controls[12] = new ControlNumericalFields();
+    }
 }

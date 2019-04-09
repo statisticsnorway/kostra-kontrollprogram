8ede24dc823e32b5398cdb7f3c8922c06651da44
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2019-04-09 11:09:24 +0200

    KOSTRAIT-75 Oppdater kontroller for skjema 11C

--- src/main/java/no/ssb/kostra/control/sensitiv/soskval/ControlRecordlengde.java
+++ src/main/java/no/ssb/kostra/control/sensitiv/soskval/ControlRecordlengde.java
@@ -3,61 +3,53 @@ package no.ssb.kostra.control.sensitiv.soskval;
 /*
  *
  */
- 
-import java.util.Vector;
+
 import no.ssb.kostra.control.Constants;
 
+import java.util.Vector;
+
 public final class ControlRecordlengde extends no.ssb.kostra.control.Control
-    implements no.ssb.kostra.control.SingleRecordErrorReport
-{
-  private final String ERROR_TEXT = "K1: Recordlengde";
-  private Vector<Integer> lineNumbers = new Vector<Integer>();
-  private final int RECORD_LENGTH = 167;
+        implements no.ssb.kostra.control.SingleRecordErrorReport {
+    private final String ERROR_TEXT = "K1: Recordlengde";
+    private final int RECORD_LENGTH = 167;
+    private Vector<Integer> lineNumbers = new Vector<Integer>();
 
-  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet)
-  {
-    boolean lineHasError = line.length() != RECORD_LENGTH; 
-  
-    if (lineHasError)
-    {
-      lineNumbers.add (new Integer (lineNumber));
-    }
-    return lineHasError;
-  }
+    public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
+        boolean lineHasError = line.length() != RECORD_LENGTH;
 
-  public String getErrorReport (int totalLineNumber)
-  {
-    String errorReport = ERROR_TEXT + ":" + lf + lf;
-    if (foundError())
-    {
-      int numOfRecords = lineNumbers.size();
-      errorReport += "\tFeil: i " + numOfRecords + 
-      " record" + (numOfRecords == 1 ? "" : "s") + " stemmer ikke antall posisjoner med det antall som er oppgitt i filbeskrivelsen."; 
-      if (numOfRecords <= 10)
-      {
-        errorReport += lf + "\t\t(Gjelder record nr.";
-        for (int i=0; i<numOfRecords; i++)
-        {
-          errorReport += " " + lineNumbers.elementAt(i);
+        if (lineHasError) {
+            lineNumbers.add(new Integer(lineNumber));
         }
-        errorReport += ").";
-      }
+        return lineHasError;
     }
-    errorReport += lf + lf;
-    return errorReport;
-  }
 
-  public boolean foundError()
-  {
-    return lineNumbers.size() > 0;
-  }  
+    public String getErrorReport(int totalLineNumber) {
+        String errorReport = ERROR_TEXT + ":" + lf + lf;
+        if (foundError()) {
+            int numOfRecords = lineNumbers.size();
+            errorReport += "\tFeil: i " + numOfRecords +
+                    " record" + (numOfRecords == 1 ? "" : "s") + " stemmer ikke antall posisjoner med det antall som er oppgitt i filbeskrivelsen.";
+            if (numOfRecords <= 10) {
+                errorReport += lf + "\t\t(Gjelder record nr.";
+                for (int i = 0; i < numOfRecords; i++) {
+                    errorReport += " " + lineNumbers.elementAt(i);
+                }
+                errorReport += ").";
+            }
+        }
+        errorReport += lf + lf;
+        return errorReport;
+    }
 
-  public String getErrorText()
-  {
-    return  ERROR_TEXT;
-  }
+    public boolean foundError() {
+        return lineNumbers.size() > 0;
+    }
 
-  public int getErrorType() {
-    return Constants.CRITICAL_ERROR;
-  }
+    public String getErrorText() {
+        return ERROR_TEXT;
+    }
+
+    public int getErrorType() {
+        return Constants.CRITICAL_ERROR;
+    }
 }

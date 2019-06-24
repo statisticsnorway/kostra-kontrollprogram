54b5e1a71edebae3441eb48b20bc157ec8887958
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2019-06-24 13:20:32 +0200

    KOSTRAIT-143 legg til kontroller for regnskap 0M, 0N, 0P og 0Q

---
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0M/ControlInterneOverforinger.java
@@ -0,0 +1,103 @@
+package no.ssb.kostra.control.regnskap.regn0M;
+
+import no.ssb.kostra.control.Constants;
+
+final class ControlInterneOverforinger extends no.ssb.kostra.control.Control
+{
+  private int sumForControl_14_a = 0;
+  private int sumForControl_14_b = 0;
+
+  private final int MAX_VALUE = 50;
+
+  boolean lineControlled = false;
+
+  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet)
+  {
+    //Kontrollen skal ikke utfores for bydelene i Oslo.
+    //Henter regionnr. fra aktuell record (line).
+    String region_nr = RecordFields.getRegion(line);
+    if (region_nr.substring(0,4).equalsIgnoreCase("0301") && 
+        !region_nr.substring(4,6).equalsIgnoreCase("00")) {
+      return false;
+    }
+     
+    if (doControl_14_a (line))
+    {
+      lineControlled = true;
+      sumForControl_14_a += RecordFields.getBelopIntValue (line);
+    }
+    if (doControl_14_b (line))
+    {
+      lineControlled = true;
+      sumForControl_14_b += RecordFields.getBelopIntValue (line);
+    }
+    return lineControlled;
+  }
+
+  public String getErrorReport (int totalLineNumber)
+  {
+    String errorReport = "Kontroll 14, interne overføringer:" + lf + lf;
+    if (foundError())
+    {
+      errorReport += "\tFeil: Differanse mellom internkjøp og -salg (art 290 og 790): " +
+          (sumForControl_14_a + sumForControl_14_b) + "'. " +
+          "(Internkjøp: " + sumForControl_14_a + "', -salg: " + sumForControl_14_b + "')" + lf + 
+              "\tKorreksjon: Rett opp i fila slik at internkjøp og internsalg stemmer overens (margin på +/- "+MAX_VALUE+"')." + lf + lf;
+    }    
+    return errorReport;
+  }
+
+  public boolean foundError()
+  {
+    return lineControlled && 
+        (Math.abs (sumForControl_14_a + sumForControl_14_b) >= MAX_VALUE);
+  }  
+
+  private boolean doControl_14_a (String line)
+  {
+    try
+    {
+      String kontoklasse = RecordFields.getKontoklasse (line);
+      int funksjon = RecordFields.getFunksjonIntValue (line);
+      String art = RecordFields.getArt (line);
+
+      boolean rettKontoklasse = (kontoklasse.equalsIgnoreCase("0") || 
+          kontoklasse.equalsIgnoreCase("1"));
+      boolean rettFunksjon = (funksjon >= 100 && funksjon <= 899);
+      boolean rettArt = art.equalsIgnoreCase("290");
+      
+      return rettKontoklasse && rettFunksjon && rettArt;
+    }
+    catch (Exception e)
+    {
+      //Maa logges!
+      return false;
+    }    
+  }
+
+  private boolean doControl_14_b (String line)
+  {
+    try
+    {
+      String kontoklasse = RecordFields.getKontoklasse (line);
+      int funksjon = RecordFields.getFunksjonIntValue (line);
+      String art = RecordFields.getArt (line);
+
+      boolean rettKontoklasse = (kontoklasse.equalsIgnoreCase("0") || 
+          kontoklasse.equalsIgnoreCase("1"));
+      boolean rettFunksjon = (funksjon >= 100 && funksjon <= 899);
+      boolean rettArt = art.equalsIgnoreCase("790");
+      
+      return rettKontoklasse && rettFunksjon && rettArt;
+    }
+    catch (Exception e)
+    {
+      //Maa logges!
+      return false;
+    }    
+  }
+
+  public int getErrorType() {
+    return Constants.CRITICAL_ERROR;
+  }
+}
---
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0P/Control8b.java
@@ -0,0 +1,53 @@
+package no.ssb.kostra.control.regnskap.regn0P;
+
+import java.util.Vector;
+import no.ssb.kostra.control.Constants;
+
+public final class Control8b extends no.ssb.kostra.control.Control {
+	private Vector<String[]> invalidCombinations = new Vector<String[]>();
+
+	public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
+		boolean lineHasError = false;
+
+		String kontoklasse = RecordFields.getKontoklasse(line);
+		String funksjon = RecordFields.getFunksjon(line);
+
+		if (funksjon.equalsIgnoreCase("841")
+				&& !kontoklasse.equalsIgnoreCase("0")) {
+			lineHasError = true;
+			String[] container = { kontoklasse, funksjon,
+					Integer.toString(lineNumber) };
+			invalidCombinations.add(container);
+		}
+		return lineHasError;
+	}
+
+	public String getErrorReport(int totalLineNumber) {
+		String errorReport = "Kontroll 8b, kombinasjon kontoklasse og funksjon i investeringsregnskapet:"
+				+ lf + lf;
+		if (foundError()) {
+			int numOfRecords = invalidCombinations.size();
+			errorReport += "\tFeil: Funksjonen er kun tillatt brukt i investeringsregnskapet."
+					+ lf;
+			for (int i = 0; i < numOfRecords; i++) {
+				String[] container = (String[]) invalidCombinations
+						.elementAt(i);
+				errorReport += "\t\tkontoklasse: " + container[0]
+						+ " funksjon: " + container[1] + " (Record nr. "
+						+ container[2] + ")" + lf;
+			}
+		}
+		errorReport += lf
+				+ "\tKorreksjon: Rett opp til funksjon som er gyldig i driftsregnskapet, eller overfør posteringen til investeringsregnskapet."
+				+ lf + lf;
+		return errorReport;
+	}
+
+	public boolean foundError() {
+		return invalidCombinations.size() > 0;
+	}
+
+	public int getErrorType() {
+		return Constants.CRITICAL_ERROR;
+	}
+}

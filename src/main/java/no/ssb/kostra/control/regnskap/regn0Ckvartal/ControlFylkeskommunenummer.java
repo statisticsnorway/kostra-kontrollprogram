9f87e74f597e5d7ac6a996134eec1ea0bf5dc9f4
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2020-03-25 08:47:02 +0100

    KOSTRAIT-287 Oppdater 0CK for 2020

--- src/main/java/no/ssb/kostra/control/regnskap/regn0Ckvartal/ControlFylkeskommunenummer.java
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0Ckvartal/ControlFylkeskommunenummer.java
@@ -2,64 +2,35 @@ package no.ssb.kostra.control.regnskap.regn0Ckvartal;
 
 import java.util.Vector;
 import no.ssb.kostra.control.Constants;
+import no.ssb.kostra.control.regnskap.regn0Akvartal.*;
 import no.ssb.kostra.utils.RegionerKvartal;
 
 final class ControlFylkeskommunenummer extends no.ssb.kostra.control.Control
 {
-  private Vector<Integer> invalidRegions = new Vector<Integer>();
-  private Vector<Integer> divergingRegions = new Vector<Integer>();
-  private String region = "";
+  private Vector<Integer> invalidRegions = new Vector<>();
 
-  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet)
-  {
+  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
     boolean lineHasError = false;
-  
-    this.region = region;
-  
-    String fnr = RecordFields.getRegion(line);
-    
-    if (! RegionerKvartal.fylkeskommuneNrIsValid (fnr))
-    {
+    String knr = RecordFields.getRegion(line);
+
+    if (!knr.equalsIgnoreCase(region)) {
       lineHasError = true;
-      invalidRegions.add (new Integer (lineNumber));
+      invalidRegions.add(new Integer(lineNumber));
     }
-    else if (! fnr.equalsIgnoreCase(region))
-    {
-      lineHasError = true;
-      divergingRegions.add(new Integer (lineNumber));
-    }
+
     return lineHasError;
   }
 
-  public String getErrorReport (int totalLineNumber)
-  {
-    String errorReport = "Kontroll 4, fylkeskommunenummer:" + lf;
+  public String getErrorReport(int totalLineNumber) {
+    String errorReport = "Kontroll 4, Fylkeskommunenummer:" + lf;
     int numOfRecords = invalidRegions.size();
-    if (numOfRecords > 0)
-    {      
-      errorReport += lf + "\tFeil: ukjent fylkeskommunenummer i " + numOfRecords + 
-          " record" + (numOfRecords == 1 ? "" : "s") + "."; 
-      if (numOfRecords <= 10)
-      {
+    if (numOfRecords > 0) {
+      errorReport += lf + "\tFeil: ukjent fylkeskommunenummer i " + numOfRecords +
+              " record" + (numOfRecords == 1 ? "" : "s") + ".";
+      if (numOfRecords <= 10) {
         errorReport += lf + "\t\t(Gjelder record nr.";
-        for (int i=0; i<numOfRecords; i++)
-        {
+        for (int i = 0; i < numOfRecords; i++) {
           errorReport += " " + invalidRegions.elementAt(i);
-        }
-        errorReport += ").";
-      }
-    }
-    numOfRecords = divergingRegions.size();
-    if (numOfRecords > 0)
-    {
-      errorReport += lf + "\tFeil: fylkeskommunenummer i " + numOfRecords + 
-          " record" + (numOfRecords == 1 ? "" : "s") + " avviker fra oppgitt fylkeskommunenummer (" + region + ")."; 
-      if (numOfRecords <= 10)
-      {
-        errorReport += lf + "\t\t(Gjelder record nr.";
-        for (int i=0; i<numOfRecords; i++)
-        {
-          errorReport += " " + divergingRegions.elementAt(i);
         }
         errorReport += ").";
       }
@@ -68,11 +39,9 @@ final class ControlFylkeskommunenummer extends no.ssb.kostra.control.Control
     return errorReport;
   }
 
-  public boolean foundError()
-  {
-    return (invalidRegions.size() > 0 ||
-            divergingRegions.size() > 0);
-  }  
+  public boolean foundError() {
+    return (invalidRegions.size() > 0 );
+  }
 
   public int getErrorType() {
     return Constants.CRITICAL_ERROR;
--- src/main/java/no/ssb/kostra/control/regnskap/regn0Ckvartal/ControlRecordlengde.java
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0Ckvartal/ControlRecordlengde.java
@@ -1,54 +1,54 @@
 package no.ssb.kostra.control.regnskap.regn0Ckvartal;
 
-import java.util.Vector;
 import no.ssb.kostra.control.Constants;
 
-final class ControlRecordlengde extends no.ssb.kostra.control.Control 
-{
-  private Vector<Integer> lineNumbers = new Vector<Integer>();
-  private final int RECORD_LENGTH = 48;
+import java.util.Vector;
 
-  public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet)
-  {
-    boolean lineHasError = line.length() != RECORD_LENGTH;
-  
-    if (lineHasError)
-    {
-      lineNumbers.add (new Integer (lineNumber));
-    }
-    return lineHasError;
-  }
+final class ControlRecordlengde extends no.ssb.kostra.control.Control {
+    private final int RECORD_LENGTH = 48;
+    private Vector<Integer> lineNumbers = new Vector<>();
 
-  public String getErrorReport (int totalLineNumber)
-  {
-    String errorReport = "Kontroll 1, recordlengde:" + lf + lf;
-    if (foundError())
-    {
-      int numOfRecords = lineNumbers.size();
-      errorReport += "\tFeil: feil antall posisjoner i " + numOfRecords + 
-      " record" + (numOfRecords == 1 ? "" : "s") + "." + lf +
-      "\tNB! Records med feil lengde tas ikke med i videre kontroller." + lf +
-      "\tDette kan medføre at ytterligere feil oppstår." + lf;
-      if (numOfRecords <= 10)
-      {
-        errorReport += " (Gjelder record nr.";
-        for (int i=0; i<numOfRecords; i++)
-        {
-          errorReport += " " + lineNumbers.elementAt(i);
+    public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
+        boolean lineHasError = line.length() != RECORD_LENGTH;
+
+        if (lineHasError) {
+            lineNumbers.add(new Integer(lineNumber));
         }
-        errorReport += ").";
-      }
+        return lineHasError;
     }
-    errorReport += lf + lf;
-    return errorReport;
-  }
 
-  public boolean foundError()
-  {
-    return lineNumbers.size() > 0;
-  }  
+    public String getErrorReport(int totalLineNumber) {
+        StringBuffer errorReport =
+                new StringBuffer("Kontroll 1, recordlengde:" + lf + lf);
 
-  public int getErrorType() {
-    return Constants.CRITICAL_ERROR;
-  }
+        if (foundError()) {
+
+            int numOfRecords = lineNumbers.size();
+            errorReport.append("\tFeil: feil antall posisjoner i " + numOfRecords +
+                    " record" + (numOfRecords == 1 ? "" : "s") + "." + lf +
+                    "\tNB! Records med feil lengde tas ikke med i videre kontroller." + lf +
+                    "\tDette kan medføre at ytterligere feil oppstår." + lf);
+
+            if (numOfRecords <= 10) {
+                errorReport.append("\t(Gjelder record nr.");
+
+                for (int i = 0; i < numOfRecords; i++)
+                    errorReport.append(" " + lineNumbers.elementAt(i));
+
+                errorReport.append(").");
+            }
+        }
+
+        errorReport.append(lf + lf);
+
+        return errorReport.toString();
+    }
+
+    public boolean foundError() {
+        return lineNumbers.size() > 0;
+    }
+
+    public int getErrorType() {
+        return Constants.CRITICAL_ERROR;
+    }
 }

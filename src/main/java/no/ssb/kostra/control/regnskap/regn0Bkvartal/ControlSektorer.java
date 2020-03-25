899c6909708abaca3eab226e4c85a53b4b34f4ab
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2020-03-25 10:28:13 +0100

    KOSTRAIT-287 Oppdater 0BK for 2020

--- src/main/java/no/ssb/kostra/control/regnskap/regn0Bkvartal/ControlSektorer.java
+++ src/main/java/no/ssb/kostra/control/regnskap/regn0Bkvartal/ControlSektorer.java
@@ -1,71 +1,66 @@
 package no.ssb.kostra.control.regnskap.regn0Bkvartal;
 
-import java.util.Vector;
 import no.ssb.kostra.control.Constants;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
 final class ControlSektorer extends no.ssb.kostra.control.Control {
-	private String[] validArter = { "000", "070", "080", "110", "151", "152",
-			"200", "320", "355", "395", "430", "450", "499", "550", "570",
-			"610", "640", "650", "890", "900", "z", "~" };
+    private List<String> validArter = Arrays.asList(
+            "000", "070", "080", "110", "151", "152",
+            "200", "320", "355", "395",
+            "430", "450", "499", "550", "570",
+            "610", "640", "650",
+            "890", "900", "z", "~");
 
-	private Vector<String[]> invalidArter = new Vector<String[]>();
+    private Vector<String[]> invalidArter = new Vector<>();
 
-	public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
-		String art = RecordFields.getArt(line);
+    public boolean doControl(String line, int lineNumber, String region, String statistiskEnhet) {
+        String art = RecordFields.getArt(line);
 
-		// Kontrollen skal ikke foretas hvis belop = 0 og art er definert.
-		try {
-			int belop = RecordFields.getBelopIntValue(line);
-			if (belop == 0 && art.trim().length() > 0) {
-				return false;
-			}
-		} catch (Exception e) {
-			// Returnerer her ogsaa. Gir ikke mening med kontroll
-			// hvis belop ikke er angitt.
-			return false;
-		}
+        // Kontrollen skal ikke foretas hvis belop = 0 og art er definert.
+        try {
+            int belop = RecordFields.getBelopIntValue(line);
+            if (belop == 0 && art.trim().length() > 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            // Returnerer her ogsaa. Gir ikke mening med kontroll
+            // hvis belop ikke er angitt.
+            return false;
+        }
 
-		boolean lineHasError = !isValidArt(art);
+        boolean lineHasError = !validArter.contains(art);
 
-		if (lineHasError) {
-			String[] container = new String[2];
-			container[0] = Integer.toString(lineNumber);
-			container[1] = art;
-			invalidArter.add(container);
-		}
-		return lineHasError;
-	}
+        if (lineHasError) {
+            String[] container = {Integer.toString(lineNumber), art};
+            invalidArter.add(container);
+        }
+        return lineHasError;
+    }
 
-	public String getErrorReport(int totalLineNumber) {
-		String errorReport = "Kontroll 7, gyldige sektorer:" + lf + lf;
-		int numOfRecords = invalidArter.size();
-		if (numOfRecords > 0) {
-			errorReport += "\tFeil: ukjent" + (numOfRecords == 1 ? "" : "e")
-					+ " sektor" + (numOfRecords == 1 ? "" : "er") + ":" + lf;
-			for (int i = 0; i < numOfRecords; i++) {
-				String[] container = (String[]) invalidArter.elementAt(i);
-				errorReport += "\t\tsektor " + container[1] + " (Record nr. "
-						+ container[0] + ")" + lf;
-			}
-		}
-		errorReport += lf;
-		return errorReport;
-	}
+    public String getErrorReport(int totalLineNumber) {
+        String errorReport = "Kontroll 7, gyldige sektorer:" + lf + lf;
+        int numOfRecords = invalidArter.size();
+        if (numOfRecords > 0) {
+            errorReport += "\tFeil: ukjent" + (numOfRecords == 1 ? "" : "e")
+                    + " sektor" + (numOfRecords == 1 ? "" : "er") + ":" + lf;
+            for (int i = 0; i < numOfRecords; i++) {
+                String[] container = invalidArter.elementAt(i);
+                errorReport += "\t\tsektor " + container[1] + " (Record nr. "
+                        + container[0] + ")" + lf;
+            }
+        }
+        errorReport += lf;
+        return errorReport;
+    }
 
-	public boolean foundError() {
-		return invalidArter.size() > 0;
-	}
+    public boolean foundError() {
+        return invalidArter.size() > 0;
+    }
 
-	private boolean isValidArt(String art) {
-		for (int i = validArter.length - 1; i >= 0; i--) {
-			if (art.equalsIgnoreCase(validArter[i])) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public int getErrorType() {
-		return Constants.NORMAL_ERROR;
-	}
+    public int getErrorType() {
+        return Constants.NORMAL_ERROR;
+    }
 }

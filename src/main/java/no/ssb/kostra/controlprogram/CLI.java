8db389f1e5d991cd9a3018147b5630d5b48ef116
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2020-11-17 11:32:37 +0100

    Fjern bruk av List.contains() pga. at f.eks 0A kan blandes med 0AK*

--- src/main/java/no/ssb/kostra/controlprogram/CLI.java
+++ src/main/java/no/ssb/kostra/controlprogram/CLI.java
@@ -3,6 +3,7 @@ package no.ssb.kostra.controlprogram;
 import no.ssb.kostra.control.Constants;
 import no.ssb.kostra.control.ErrorReport;
 import no.ssb.kostra.control.ErrorReportEntry;
+import no.ssb.kostra.control.felles.Comparator;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -16,42 +17,46 @@ public class CLI {
         try {
             Arguments arguments = new Arguments(args);
 
-            if (List.of("0AK1", "0AK2", "0AK3", "0AK4",
-                    "0BK1", "0BK2", "0BK3", "0BK4",
-                    "0CK1", "0CK2", "0CK3", "0CK4",
-                    "0DK1", "0DK2", "0DK3", "0DK4").contains(arguments.getSkjema())) {
+            if (Comparator.isCodeInCodelist(
+                    arguments.getSkjema()
+                    , List.of("0AK1", "0AK2", "0AK3", "0AK4",
+                            "0BK1", "0BK2", "0BK3", "0BK4",
+                            "0CK1", "0CK2", "0CK3", "0CK4",
+                            "0DK1", "0DK2", "0DK3", "0DK4"))) {
                 er = no.ssb.kostra.control.regnskap.kvartal.Main.doControls(arguments);
 
-            } else if (List.of("0A", "0B", "0C", "0D",
-                    "0I", "0J", "0K", "0L",
-                    "0M", "0N", "0P", "0Q").contains(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(
+                    arguments.getSkjema()
+                    , List.of("0A", "0B", "0C", "0D",
+                            "0I", "0J", "0K", "0L",
+                            "0M", "0N", "0P", "0Q"))) {
                 er = no.ssb.kostra.control.regnskap.kostra.Main.doControls(arguments);
 
-            } else if (List.of("0F", "0G").contains(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("0F", "0G"))) {
                 er = no.ssb.kostra.control.regnskap.kirkekostra.Main.doControls(arguments);
 
-            } else if (List.of("0X", "0Y").contains(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("0X", "0Y"))) {
                 er = no.ssb.kostra.control.regnskap.helseforetak.Main.doControls(arguments);
 
-            } else if ("11F".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("11F"))) {
                 er = no.ssb.kostra.control.sosial.s11_sosialhjelp.Main.doControls(arguments);
 
-            } else if ("11CF".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("11CF"))) {
                 er = no.ssb.kostra.control.sosial.s11c_kvalifiseringsstonad.Main.doControls(arguments);
 
-            } else if ("15F".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("15F"))) {
                 er = no.ssb.kostra.control.barnevern.s15.Main.doControls(arguments);
 
-            } else if ("52AF".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("52AF"))) {
                 er = no.ssb.kostra.control.famvern.s52a.Main.doControls(arguments);
 
-            } else if ("52BF".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("52BF"))) {
                 er = no.ssb.kostra.control.famvern.s52b.Main.doControls(arguments);
 
-            } else if ("53F".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("53F"))) {
                 er = no.ssb.kostra.control.famvern.s53.Main.doControls(arguments);
 
-            } else if ("55F".equalsIgnoreCase(arguments.getSkjema())) {
+            } else if (Comparator.isCodeInCodelist(arguments.getSkjema(), List.of("55F"))) {
                 er = no.ssb.kostra.control.famvern.s55.Main.doControls(arguments);
 
             } else {
@@ -82,6 +87,7 @@ public class CLI {
             error_type_found = Constants.PARAMETER_ERROR;
 
         } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
             error_type_found = Constants.CRITICAL_ERROR;
 
         } catch (Exception e) {

a59d6519ae4aa69a30bba40a60c0ae94bfb62f79
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2021-07-09 15:24:27 +0200

    KOSPRO-54 legg til klasse for akseptansetesting av tekststrenger

--- src/main/java/no/ssb/kostra/utils/TestStringInputAndResult.java
+++ src/main/java/no/ssb/kostra/utils/TestStringInputAndResult.java
@@ -1,2 +1,46 @@
-package no.ssb.kostra.utils;public class TestStringInputAndResult {
+package no.ssb.kostra.utils;
+
+import no.ssb.kostra.controlprogram.Arguments;
+import no.ssb.kostra.felles.ErrorReport;
+
+public class TestStringInputAndResult {
+    private final Arguments arguments;
+    private final String string;
+    private final boolean result;
+    private final ErrorReport errorReport;
+    private final int expectedErrorType;
+
+    public TestStringInputAndResult(Arguments arguments, String string, boolean result, int expectedErrorType) {
+        this.arguments = arguments;
+        this.errorReport = new ErrorReport(arguments);
+        this.string = string;
+        this.result = result;
+        this.expectedErrorType = expectedErrorType;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public ErrorReport getErrorReport() {
+        return errorReport;
+    }
+
+    public int getExpectedErrorType() {
+        return expectedErrorType;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "result=" + result +
+                ", expectedErrorType=" + expectedErrorType +
+                ", arguments=" + arguments +
+                ", string=" + string +
+                '}';
+    }
 }

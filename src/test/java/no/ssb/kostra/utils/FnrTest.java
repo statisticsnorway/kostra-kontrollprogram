89287b3aaf82f67e4a1e7613eaac87521db02e82
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2021-07-09 15:23:05 +0200

    KOSPRo-54 legg til test for utledning av alder fra FNR

--- src/test/java/no/ssb/kostra/utils/FnrTest.java
+++ src/test/java/no/ssb/kostra/utils/FnrTest.java
@@ -2,30 +2,37 @@ package no.ssb.kostra.utils;
 
 import org.junit.Test;
 
+
+import static no.ssb.kostra.utils.Fnr.*;
 import static org.junit.Assert.*;
 
 public class FnrTest {
     @Test
     public void isValidDateTest() {
-        assertTrue(Fnr.isValidDate("010112", "ddMMyy"));
-        assertTrue(Fnr.isValidDate("01012012", "ddMMyyyy"));
-        assertFalse(Fnr.isValidDate("30022012", "ddMMyyyy"));
+        assertTrue(isValidDate("010112", "ddMMyy"));
+        assertTrue(isValidDate("01012012", "ddMMyyyy"));
+        assertFalse(isValidDate("30022012", "ddMMyyyy"));
     }
 
     @Test
     public void isValidNorwIdTest() {
-        assertTrue(Fnr.isValidNorwId("01010150589"));
-        assertTrue(Fnr.isValidNorwId("41010150572"));
-        assertFalse(Fnr.isValidNorwId("01011200100"));
-        assertFalse(Fnr.isValidNorwId("01011200200"));
-        assertFalse(Fnr.isValidNorwId("01011255555"));
-        assertFalse(Fnr.isValidNorwId("01011299999"));
+        assertTrue(isValidNorwId("01010150589"));
+        assertTrue(isValidNorwId("41010150572"));
+        assertFalse(isValidNorwId("01011200100"));
+        assertFalse(isValidNorwId("01011200200"));
+        assertFalse(isValidNorwId("01011255555"));
+        assertFalse(isValidNorwId("01011299999"));
     }
 
     @Test
     public void isValidDUFnrTest() {
-        assertTrue(Fnr.isValidDUFnr("201212345603"));
-        assertFalse(Fnr.isValidDUFnr("201234567890"));
-        assertFalse(Fnr.isValidDUFnr("12345678901"));
+        assertTrue(isValidDUFnr("201212345603"));
+        assertFalse(isValidDUFnr("201234567890"));
+        assertFalse(isValidDUFnr("12345678901"));
+    }
+
+    @Test
+    public void getAlderFromFnrTest() {
+        assertEquals(20, getAlderFromFnr("01010150589", 2021));
     }
 }

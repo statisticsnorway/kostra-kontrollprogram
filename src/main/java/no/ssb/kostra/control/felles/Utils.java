8fa047a9945c6ca720ce0f3f0a1f3596dbfb59a1
Hagemo, Jon Ole Johansen <jon.ole.johansen.hagemo@ssb.no>
2021-11-30 03:56:39 +0100

    #patch lagt til linjenummerering

--- src/main/java/no/ssb/kostra/control/felles/Utils.java
+++ src/main/java/no/ssb/kostra/control/felles/Utils.java
@@ -1,18 +1,38 @@
 package no.ssb.kostra.control.felles;
 
+import no.ssb.kostra.controlprogram.Arguments;
 import no.ssb.kostra.felles.Code;
 import no.ssb.kostra.felles.FieldDefinition;
 import no.ssb.kostra.felles.Record;
-import no.ssb.kostra.controlprogram.Arguments;
 import no.ssb.kostra.utils.Format;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Utils {
-    public static String createLinenumber(int l, Record r){
+    public static String createLinenumber(int l, Record r) {
         return "Linje " + Format.sprintf("%0" + l + "d", r.getLine()) + " : " + r.getRecord().trim();
+    }
+
+    public static List<Record> addLineNumbering(List<Record> records) {
+        AtomicInteger lineNumber = new AtomicInteger(0);
+
+        return records.stream()
+                .peek(r -> r.setFieldAsInteger("linjenummer", lineNumber.incrementAndGet()))
+                .peek(r -> r.setFieldAsInteger("LINJENUMMER", lineNumber.get()))
+                .collect(Collectors.toList());
+    }
+
+    public static Record addLineNumbering(Record record) {
+        AtomicInteger lineNumber = new AtomicInteger(0);
+
+        record.setFieldAsInteger("linjenummer", lineNumber.incrementAndGet());
+        record.setFieldAsInteger("LINJENUMMER", lineNumber.get());
+
+        return record;
     }
 
     public static List<Record> getValidRecords(List<String> list1, List<FieldDefinition> fieldDefinitions) {
@@ -23,19 +43,19 @@ public class Utils {
                 .collect(Collectors.toList());
     }
 
-    public static List<FieldDefinition> mergeFieldDefinitionsAndArguments(List<FieldDefinition> fieldDefinitions, Arguments args){
+    public static List<FieldDefinition> mergeFieldDefinitionsAndArguments(List<FieldDefinition> fieldDefinitions, Arguments args) {
         return fieldDefinitions.stream()
                 .map(d -> {
-                    if (d.getName().equalsIgnoreCase("skjema")){
+                    if (d.getName().equalsIgnoreCase("skjema")) {
                         d.setCodeList(List.of(new Code(args.getSkjema(), "Skjematype")));
 
-                    } else if (d.getName().equalsIgnoreCase("aargang")){
+                    } else if (d.getName().equalsIgnoreCase("aargang")) {
                         d.setCodeList(List.of(new Code(args.getAargang(), "Årgang")));
 
-                    } else if (d.getName().equalsIgnoreCase("region")){
+                    } else if (d.getName().equalsIgnoreCase("region")) {
                         d.setCodeList(List.of(new Code(args.getRegion(), "Region")));
 
-                    } else if (d.getName().equalsIgnoreCase("orgnr")){
+                    } else if (d.getName().equalsIgnoreCase("orgnr")) {
                         List<Code> list = Stream.of(args.getOrgnr().split(","))
                                 .collect(Collectors.toList())
                                 .stream()
@@ -44,7 +64,7 @@ public class Utils {
 
                         d.setCodeList(list);
 
-                    } else if (d.getName().equalsIgnoreCase("foretaksnr")){
+                    } else if (d.getName().equalsIgnoreCase("foretaksnr")) {
                         List<Code> list = Stream.of(args.getForetaknr().concat(",").concat(args.getOrgnr()).split(","))
                                 .collect(Collectors.toList())
                                 .stream()
@@ -59,14 +79,14 @@ public class Utils {
                 .collect(Collectors.toList());
     }
 
-    public static List<String> rpadList(List<String> list, int width){
+    public static List<String> rpadList(List<String> list, int width) {
         return list.stream()
                 // rightPad / legger til mellomrom på slutten av tekstene slik at alle blir (width) tegn lange
                 .map(c -> String.format("%1$-" + width + "s", c))
                 .collect(Collectors.toList());
     }
 
-    public static String replaceSpaceWithNoBreakingSpace(String s){
+    public static String replaceSpaceWithNoBreakingSpace(String s) {
         return s.replace(" ", "&nbsp;");
     }
 
--- src/test/groovy/no/ssb/kostra/control/felles/UtilsSpec.groovy
+++ src/test/groovy/no/ssb/kostra/control/felles/UtilsSpec.groovy
@@ -1,4 +1,32 @@
 package no.ssb.kostra.control.felles
 
-class UtilsSpec {
+import no.ssb.kostra.control.regnskap.FieldDefinitions
+import no.ssb.kostra.felles.FieldDefinition
+import no.ssb.kostra.felles.Record
+import spock.lang.Specification
+
+class UtilsSpec extends Specification {
+    private static final List<FieldDefinition> definitions = FieldDefinitions.getFieldDefinitions();
+
+    def "should add linenumbering"() {
+        given:
+        def recordList = new ArrayList<Record>()
+
+        Map recordMap = new HashMap<String, String>()
+        recordMap.putAll(Map.of("skjema", skjema, "kontoklasse", kontoklasse, "funksjon_kapittel", funksjon))
+        recordList.add(new Record(recordMap, definitions))
+
+        when:
+        def recordListWithLinenumbers = Utils.addLineNumbering(recordList)
+        def record = (Record) recordListWithLinenumbers.get(0)
+        def testResult = record.getFieldAsInteger("linjenummer")
+
+        then:
+        testResult == expectedResult
+
+        where:
+        skjema | kontoklasse | funksjon || expectedResult
+        "0A"   | "1"         | "100 "   || 1
+        "0A"   | "1"         | "841 "   || 1
+    }
 }

8a10a19691c1e63482d727ecbbd8b7e8baa5f1b9
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-11-04 09:19:04 +0100

    Added <sonar.moduleKey>${project.groupId}:${project.artifactId}</sonar.moduleKey>

--- web/src/main/vite-project/cypress.config.ts
+++ web/src/main/vite-project/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: "http://localhost:5174",
+    baseUrl: "http://localhost:5173",
   },
 
   component: {

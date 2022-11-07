5e5259f5552980e106e4c837a785ee723d15aa80
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-11-07 21:07:58 +0100

    Added Bootstrap 5 + Sass

--- web/src/main/vite-project/src/App.tsx
+++ web/src/main/vite-project/src/App.tsx
@@ -1,27 +1,44 @@
 import { useState } from 'react'
 // @ts-ignore
-import reactLogo from "/src/assets/react.svg"
-import './App.css'
+import reactLogo from './assets/react.svg'
+import './App.scss'
+import { Button } from 'react-bootstrap'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return <div className="App">
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
-    <h1>Vite + React</h1>
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
-  </div>
+  return (
+      <div className="App">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React with Typescript + Bootstrap 5</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button>{' '}
+        <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button>{' '}
+        <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+      </div>
+  );
 }
 
-export default App
+export default App;
--- web/src/main/vite-project/src/api/apiCalls.test.ts
+++ web/src/main/vite-project/src/api/apiCalls.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, it, vi} from 'vitest'
+import {describe, expect, it, vi} from "vitest"
 import {api, kontrollerSkjemaAsync, listSkjemaTyperAsync} from "./apiCalls"
 import {ErrorReportVm, KostraFormTypeVm, KostraFormVm} from "../kostraTypes"
 

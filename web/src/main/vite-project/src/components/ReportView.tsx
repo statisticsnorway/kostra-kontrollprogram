2a2a296d16da5f099e299b7b009d467f481fc117
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-11-29 10:33:01 +0100

    Feilliste vises nå i et card

--- web/src/main/vite-project/src/components/ReportView.tsx
+++ web/src/main/vite-project/src/components/ReportView.tsx
@@ -123,28 +123,37 @@ export const ReportView = (props: {
         </ul>
     </div>
 
-    const renderErrorDetailsTable = () => <div className="table-responsive mt-4">
-        <table className="table table-striped table-sm">
-            <thead>
-            <tr>
-                <th scope="col">Feilkode</th>
-                <th scope="col">Journalnummer</th>
-                <th scope="col">Saksbehandler</th>
-                <th scope="col">Kontrolltype</th>
-                <th scope="col">Kontrolltekst</th>
-            </tr>
-            </thead>
-            <tbody>
-            {fileReport.feil.map((reportEntry, index) =>
-                <tr key={index}>
-                    <th scope="row">{renderErrorLevel(reportEntry.feilkode)}</th>
-                    <td>{reportEntry.journalnummer}</td>
-                    <td>{reportEntry.saksbehandler}</td>
-                    <td>{reportEntry.kontrollnummer}</td>
-                    <td>{reportEntry.kontrolltekst}</td>
-                </tr>)}
-            </tbody>
-        </table>
+    const renderErrorDetailsTable = () => <div className="card mt-3">
+        <div className="card-body">
+            <h5 className="card-title mb-0">Feilliste</h5>
+        </div>
+        <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+                <div className="table-responsive">
+                    <table className="table table-striped table-sm">
+                        <thead>
+                        <tr>
+                            <th scope="col">Feilkode</th>
+                            <th scope="col">Journalnummer</th>
+                            <th scope="col">Saksbehandler</th>
+                            <th scope="col">Kontrolltype</th>
+                            <th scope="col">Kontrolltekst</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {fileReport.feil.map((reportEntry, index) =>
+                            <tr key={index}>
+                                <th scope="row">{renderErrorLevel(reportEntry.feilkode)}</th>
+                                <td>{reportEntry.journalnummer}</td>
+                                <td>{reportEntry.saksbehandler}</td>
+                                <td>{reportEntry.kontrollnummer}</td>
+                                <td>{reportEntry.kontrolltekst}</td>
+                            </tr>)}
+                        </tbody>
+                    </table>
+                </div>
+            </li>
+        </ul>
     </div>
 
     return <>

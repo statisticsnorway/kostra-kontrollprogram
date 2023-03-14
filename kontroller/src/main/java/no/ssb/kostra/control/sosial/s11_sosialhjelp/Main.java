33ebfb910d37aea961d714fb986cd1e0028d242e
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-03-14 08:21:55 +0100

    Sosial: Lagt til kontroll 21

--- kontroller/src/main/java/no/ssb/kostra/control/sosial/s11_sosialhjelp/Main.java
+++ kontroller/src/main/java/no/ssb/kostra/control/sosial/s11_sosialhjelp/Main.java
@@ -491,6 +491,34 @@ public class Main {
         );
     }
 
+
+    public static boolean control21ViktigsteKildeTilLivsopphold(
+            final ErrorReport errorReport, final KostraRecord record) {
+
+        errorReport.incrementCount();
+
+        return ControlFelt1InneholderKodeFraKodelisteSaaFelt2InneholderKodeFraKodeliste.doControl(
+                errorReport
+                , new ErrorReportEntry(
+                        record.getFieldAsString(SAKSBEHANDLER)
+                        , record.getFieldAsString(PERSON_JOURNALNR)
+                        , record.getFieldAsString(PERSON_FODSELSNR)
+                        , " "
+                        , "Kontroll 21 Viktigste kilde til livsopphold i relasjon til arbeidssituasjon. "
+                        + record.getFieldDefinitionByName(VKLO).getCodeList().stream().filter(c -> Comparator.isCodeInCodeList(c.getCode(), List.of("5"))).map(Code::getValue).collect(Collectors.joining("")) + "."
+                        , "Mottakerens viktigste kilde til livsopphold ved siste kontakt med sosial-/NAV-kontoret er "
+                        + record.getFieldDefinitionByName(VKLO).getCodeList().stream().filter(c -> Comparator.isCodeInCodeList(c.getCode(), List.of("5"))).map(Code::getValue).collect(Collectors.joining("")) + ". "
+                        + "Arbeidssituasjonen er '" + record.getFieldAsTrimmedString(ARBSIT) + "', forventet én av '"
+                        + record.getFieldDefinitionByName(ARBSIT).getCodeList().stream().map(Code::toString).toList()
+                        + "'. Feltet er obligatorisk å fylle ut."
+                        , Constants.NORMAL_ERROR
+                )
+                , record.getFieldAsString(VKLO)
+                , List.of("5")
+                , record.getFieldAsString(ARBSIT)
+                , List.of("02", "03", "04", "05", "06", "07", "08")
+        );
+    }
     public static boolean control22TilknytningTilTrygdesystemetOgAlder(
             final ErrorReport errorReport, final KostraRecord record) {
 

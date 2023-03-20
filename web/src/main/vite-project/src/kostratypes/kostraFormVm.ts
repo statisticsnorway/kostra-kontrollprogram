829eed1259d2b66f6046c4eb336163179354c765
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-03-20 21:53:02 +0100

    Removed wrong nullable

--- web/src/main/vite-project/src/kostratypes/kostraFormVm.ts
+++ web/src/main/vite-project/src/kostratypes/kostraFormVm.ts
@@ -1,11 +1,10 @@
 import CompanyIdVm from "./companyIdVm"
-import {Nullable} from "vitest";
 
 interface KostraFormVm {
     aar: NonNullable<number>
     skjema: NonNullable<string>
     region: NonNullable<string>
-    orgnrForetak: Nullable<string>
+    orgnrForetak: string | undefined
     orgnrVirksomhet: CompanyIdVm[] | undefined
     skjemaFil: FileList
 }

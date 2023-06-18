59cf2d1257adf40fc3d16c7de4681811ce9b24dc
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-06-18 16:56:12 +0200

    Removed unused method

--- kontroller/src/main/kotlin/no/ssb/kostra/area/sosial/kvalifisering/FieldDefinitionExtensions.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/area/sosial/kvalifisering/FieldDefinitionExtensions.kt
@@ -2,8 +2,6 @@ package no.ssb.kostra.area.sosial.kvalifisering
 
 import no.ssb.kostra.program.FieldDefinition
 
-fun FieldDefinition.getCodes() = this.codeList.map { it.code }
-
 fun FieldDefinition.codeListToString() = this.codeList.map { it.toString() }
 
 fun FieldDefinition.codeIsMissing(code: String) = this.codeList.none { it.code == code }

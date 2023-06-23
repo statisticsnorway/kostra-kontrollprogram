7770744fe38016dec204c654e3ed58207a822588
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-06-24 00:20:03 +0200

    Simplified chain in Rule113SummeringTilskudd

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/regnskap/kirkekostra/Rule113SummeringTilskudd.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/regnskap/kirkekostra/Rule113SummeringTilskudd.kt
@@ -4,7 +4,6 @@ import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_ART
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_BELOP
 import no.ssb.kostra.program.KostraRecord
 import no.ssb.kostra.validation.report.Severity
-import no.ssb.kostra.validation.report.ValidationReportEntry
 import no.ssb.kostra.validation.rule.AbstractRecordRule
 import no.ssb.kostra.validation.rule.regnskap.kostra.extensions.isBevilgningDriftRegnskap
 
@@ -12,22 +11,13 @@ class Rule113SummeringTilskudd : AbstractRecordRule(
     "Kontroll 113 : Summeringskontroller driftsregnskapet, tilskudd",
     Severity.ERROR
 ) {
-    override fun validate(context: List<KostraRecord>): List<ValidationReportEntry>? = context
+    override fun validate(context: List<KostraRecord>) = context
         .filter {
             it.isBevilgningDriftRegnskap()
-        }
-        .takeIf {
-            it.any()
-        }
-        ?.filter {
-            it.getFieldAsString(FIELD_ART) == "830"
-        }
-        ?.sumOf {
-            it.getFieldAsIntegerDefaultEquals0(FIELD_BELOP)
-        }
-        ?.takeUnless {
-            0 < it
-        }
+                    && it.getFieldAsString(FIELD_ART) == "830"
+        }.takeIf { it.any() }
+        ?.sumOf { it.getFieldAsIntegerDefaultEquals0(FIELD_BELOP) }
+        ?.takeUnless { 0 < it }
         ?.let { tilskudd ->
             createSingleReportEntryList(
                 messageText = "Korrigér slik at fila inneholder tilskudd ($tilskudd) fra kommunen"

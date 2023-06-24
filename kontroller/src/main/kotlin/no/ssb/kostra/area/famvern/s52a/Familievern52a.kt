4074ab9662a543077dc110d00f7b0695d4ea2e29
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-06-24 15:50:18 +0200

    Added Familievern52a.kt to sonarcloud.exclusions

---
+++ kontroller/src/main/kotlin/no/ssb/kostra/area/famvern/s52a/Familievern52a.kt
@@ -0,0 +1,4 @@
+package no.ssb.kostra.area.famvern.s52a
+
+class Familievern52a() {
+}
--- kontroller/src/main/kotlin/no/ssb/kostra/area/famvern/s52a/Main.kt
+++
@@ -1,7 +0,0 @@
-package no.ssb.kostra.area.famvern.s52a
-
-import no.ssb.kostra.program.KotlinArguments
-
-class Famvern52a(arguments: KotlinArguments) {
-
-}

7a6a3d1aed1d0376dcf98420660707fec69c82c5
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-06-24 14:03:34 +0200

    Removed redundant, unused override #2

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/AbstractStringRule.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/AbstractStringRule.kt
@@ -1,9 +1,6 @@
 package no.ssb.kostra.validation.rule
 
 import no.ssb.kostra.validation.report.Severity
-import no.ssb.kostra.validation.report.ValidationReportEntry
 
 abstract class AbstractStringRule(ruleName: String, severity: Severity) :
-    AbstractRule<String>(ruleName, severity) {
-    override fun validate(context: List<String>): List<ValidationReportEntry>? = null
-}
+    AbstractRule<String>(ruleName, severity)

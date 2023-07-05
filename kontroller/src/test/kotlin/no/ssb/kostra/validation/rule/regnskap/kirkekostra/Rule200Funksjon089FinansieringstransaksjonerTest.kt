03a81f342e6fb2614946b6ebbf981a77071ae456
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-07-05 20:52:56 +0200

    Completed Rule190MemoriakontiTest

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kirkekostra/Rule200Funksjon089FinansieringstransaksjonerTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kirkekostra/Rule200Funksjon089FinansieringstransaksjonerTest.kt
@@ -5,6 +5,7 @@ import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_ART
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_FUNKSJON
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_KONTOKLASSE
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_SKJEMA
+import no.ssb.kostra.program.KostraRecord
 import no.ssb.kostra.validation.report.Severity
 import no.ssb.kostra.validation.rule.ForAllRowItem
 import no.ssb.kostra.validation.rule.KostraTestFactory.validationRuleTest
@@ -18,29 +19,29 @@ class Rule200Funksjon089FinansieringstransaksjonerTest : BehaviorSpec({
             forAllRows = listOf(
                 ForAllRowItem(
                     "isBevilgningRegnskap = true, funksjon = 089, art != 500..580",
-                    kostraRecordsInTest("0F", "3", "089 ", "499"),
+                    kostraRecordsInTest(skjema = "0F", funksjon = "089 ", art = "499"),
                     expectedErrorMessage = "Korrigér i fila slik at art (499) " +
                             "er gyldig mot funksjon 089. Gyldige arter er 500-580, 830 og 900-980."
                 ),
                 ForAllRowItem(
                     "isBevilgningRegnskap = false, funksjon = 089, art != 500..580",
-                    kostraRecordsInTest("0X", "3", "089 ", "499")
+                    kostraRecordsInTest("0X", "089 ", "499")
                 ),
                 ForAllRowItem(
                     "isBevilgningRegnskap = true, funksjon != 089, art != 500..580",
-                    kostraRecordsInTest("0F", "3", "088 ", "499")
+                    kostraRecordsInTest("0F", "088 ", "499")
                 ),
                 ForAllRowItem(
                     "isBevilgningRegnskap = true, funksjon = 089, art = 500",
-                    kostraRecordsInTest("0F", "3", "089 ", "500")
+                    kostraRecordsInTest("0F", "089 ", "500")
                 ),
                 ForAllRowItem(
                     "isBevilgningRegnskap = true, funksjon = 089, art = 830",
-                    kostraRecordsInTest("0F", "3", "089 ", "830")
+                    kostraRecordsInTest("0F", "089 ", "830")
                 ),
                 ForAllRowItem(
                     "isBevilgningRegnskap = true, funksjon = 089, art = 900",
-                    kostraRecordsInTest("0F", "3", "089 ", "900")
+                    kostraRecordsInTest("0F", "089 ", "900")
                 )
             ),
             expectedSeverity = Severity.ERROR,
@@ -49,16 +50,15 @@ class Rule200Funksjon089FinansieringstransaksjonerTest : BehaviorSpec({
     )
 }) {
     companion object {
-        fun kostraRecordsInTest(
+        private fun kostraRecordsInTest(
             skjema: String,
-            kontoklasse: String,
             funksjon: String,
             art: String
-        ) = mapOf(
+        ): List<KostraRecord> = mapOf(
             FIELD_SKJEMA to skjema,
-            FIELD_KONTOKLASSE to kontoklasse,
+            FIELD_KONTOKLASSE to "3",
             FIELD_FUNKSJON to funksjon,
-            FIELD_ART to art,
+            FIELD_ART to art
         ).toKostraRecord().asList()
     }
 }
--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kostra/Rule190MemoriakontiTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kostra/Rule190MemoriakontiTest.kt
@@ -1,253 +1,70 @@
 package no.ssb.kostra.validation.rule.regnskap.kostra
 
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.data.forAll
-import io.kotest.data.row
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_BELOP
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_KAPITTEL
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_KONTOKLASSE
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_SEKTOR
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_SKJEMA
 import no.ssb.kostra.validation.report.Severity
-import no.ssb.kostra.validation.rule.TestUtils.verifyValidationResult
-import no.ssb.kostra.validation.rule.regnskap.RegnskapTestUtils.toKostraRecords
+import no.ssb.kostra.validation.rule.ForAllRowItem
+import no.ssb.kostra.validation.rule.KostraTestFactory.validationRuleTest
+import no.ssb.kostra.validation.rule.regnskap.RegnskapTestUtils.toKostraRecord
+
 
 class Rule190MemoriakontiTest : BehaviorSpec({
-    Given("context") {
-        val sut = Rule190Memoriakonti()
-
-        forAll(
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0B",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
+    include(
+        validationRuleTest(
+            sut = Rule190Memoriakonti(),
+            forAllRows = listOf(
+                ForAllRowItem(
+                    "isBalanseRegnskap = true, kapittel matching, kapittel #2 matching, sum belop matching",
+                    listOf(
+                        kostraRecordInTest("0B", "9100", "000", "100"),
+                        kostraRecordInTest("0B", "9999", "990", "-1000")
                     ),
-                    mapOf(
-                        FIELD_SKJEMA to "0B",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-100"
+                    expectedErrorMessage = "Korrigér i fila slik at differansen (-900) mellom memoriakontiene (100) " +
+                            "og motkonto for memoriakontiene (-1000) går i 0. (margin på +/- 10')"
+                ),
+                ForAllRowItem(
+                    "isBalanseRegnskap = false, kapittel matching, kapittel #2 matching, sum belop matching",
+                    listOf(
+                        kostraRecordInTest("0A", "9100", "000", "100"),
+                        kostraRecordInTest("0A", "9999", "990", "-1000")
                     )
-                ), false
+                ),
+                ForAllRowItem(
+                    "isBalanseRegnskap = true, kapittel not matching, kapittel #2 matching, sum belop not matching",
+                    listOf(
+                        kostraRecordInTest("0B", "9099", "000", "100"),
+                        kostraRecordInTest("0B", "9999", "990", "-10")
+                    )
+                ),
+                ForAllRowItem(
+                    "isBalanseRegnskap = true, kapittel matching, kapittel #2 matching, sum belop not matching",
+                    listOf(
+                        kostraRecordInTest("0B", "9100", "000", "100"),
+                        kostraRecordInTest("0B", "9999", "990", "-100")
+                    )
+                ),
             ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0B",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0B",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-1000"
-                    )
-                ), true
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0D",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0D",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-100"
-                    )
-                ), false
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0D",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0D",
-                        FIELD_KONTOKLASSE to "2",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-1000"
-                    )
-                ), true
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0J",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0J",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-100"
-                    )
-                ), false
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0J",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0J",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-1000"
-                    )
-                ), true
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0L",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0L",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-100"
-                    )
-                ), false
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0L",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0L",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-1000"
-                    )
-                ), true
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0N",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0N",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-100"
-                    )
-                ), false
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0N",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0N",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-1000"
-                    )
-                ), true
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0Q",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0Q",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-100"
-                    )
-                ), false
-            ),
-            row(
-                listOf(
-                    mapOf(
-                        FIELD_SKJEMA to "0Q",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9100",
-                        FIELD_SEKTOR to "000",
-                        FIELD_BELOP to "100"
-                    ),
-                    mapOf(
-                        FIELD_SKJEMA to "0Q",
-                        FIELD_KONTOKLASSE to "5",
-                        FIELD_KAPITTEL to "9999",
-                        FIELD_SEKTOR to "990",
-                        FIELD_BELOP to "-1000"
-                    )
-                ), true
-            )
-        ) { recordList, expectError ->
-            val kostraRecordList = recordList.toKostraRecords()
-            val memoriakonti = kostraRecordList[0].fieldAsIntOrDefault(FIELD_BELOP)
-            val motpostMemoriakonti = kostraRecordList[1].fieldAsIntOrDefault(FIELD_BELOP)
-
-            When("List is $recordList") {
-                verifyValidationResult(
-                    validationReportEntries = sut.validate(kostraRecordList),
-                    expectError = expectError,
-                    expectedSeverity = Severity.WARNING,
-                    "Korrigér i fila slik at differansen (${memoriakonti + motpostMemoriakonti}) " +
-                            "mellom memoriakontiene ($memoriakonti) og motkonto for memoriakontiene " +
-                            "($motpostMemoriakonti) går i 0. (margin på +/- 10')"
-                )
-            }
-        }
+            expectedSeverity = Severity.WARNING,
+            useArguments = false
+        )
+    )
+}) {
+    companion object {
+        private fun kostraRecordInTest(
+            skjema: String,
+            kapittel: String,
+            sektor: String,
+            belop: String
+        ) = mapOf(
+            FIELD_SKJEMA to skjema,
+            FIELD_KONTOKLASSE to "2",
+            FIELD_KAPITTEL to kapittel,
+            FIELD_SEKTOR to sektor,
+            FIELD_BELOP to belop
+        ).toKostraRecord()
     }
-})
+}

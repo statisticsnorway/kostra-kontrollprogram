7b51326562f852fb394bffb1823bd8bc7e347052
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-07-06 19:18:28 +0200

    Updated Rule100SummeringDriftUtgiftsposteringerTest

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kostra/Rule100SummeringDriftUtgiftsposteringerTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kostra/Rule100SummeringDriftUtgiftsposteringerTest.kt
@@ -1,8 +1,6 @@
 package no.ssb.kostra.validation.rule.regnskap.kostra
 
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.data.forAll
-import io.kotest.data.row
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_ART
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_BELOP
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_FUNKSJON
@@ -10,52 +8,60 @@ import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_KONTOKLASSE
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_REGION
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_SKJEMA
 import no.ssb.kostra.validation.report.Severity
-import no.ssb.kostra.validation.rule.TestUtils.verifyValidationResult
+import no.ssb.kostra.validation.rule.ForAllRowItem
+import no.ssb.kostra.validation.rule.KostraTestFactory.validationRuleTest
 import no.ssb.kostra.validation.rule.regnskap.RegnskapTestUtils.asList
 import no.ssb.kostra.validation.rule.regnskap.RegnskapTestUtils.toKostraRecord
 
 class Rule100SummeringDriftUtgiftsposteringerTest : BehaviorSpec({
-    Given("context") {
-        val sut = Rule100SummeringDriftUtgiftsposteringer()
-
-        forAll(
-            row("420400", "0A", "1", "100 ", "010", "1", false),
-            row("420400", "0A", "1", "100 ", "590", "1", false),
-            row("420400", "0A", "1", "100 ", "590", "0", true),
-            row("030101", "0A", "1", "100 ", "590", "0", false),
-            row("420400", "0C", "1", "100 ", "010", "1", false),
-            row("420400", "0C", "1", "100 ", "590", "1", false),
-            row("420400", "0C", "1", "100 ", "590", "0", true),
-            row("420400", "0I", "3", "100 ", "010", "1", false),
-            row("420400", "0I", "3", "100 ", "590", "1", false),
-            row("420400", "0I", "3", "100 ", "590", "0", true),
-            row("420400", "0K", "3", "100 ", "010", "1", false),
-            row("420400", "0K", "3", "100 ", "590", "1", false),
-            row("420400", "0K", "3", "100 ", "590", "0", true),
-            row("420400", "0M", "3", "100 ", "010", "1", false),
-            row("420400", "0M", "3", "100 ", "590", "1", false),
-            row("420400", "0M", "3", "100 ", "590", "0", true),
-            row("420400", "0P", "3", "100 ", "010", "1", false),
-            row("420400", "0P", "3", "100 ", "590", "1", false),
-            row("420400", "0P", "3", "100 ", "590", "0", true)
-        ) { region, skjema, kontoklasse, funksjon, art, belop, expectError ->
-            val kostraRecordList = mapOf(
-                FIELD_REGION to region,
-                FIELD_SKJEMA to skjema,
-                FIELD_KONTOKLASSE to kontoklasse,
-                FIELD_FUNKSJON to funksjon,
-                FIELD_ART to art,
-                FIELD_BELOP to belop
-            ).toKostraRecord().asList()
-
-            When("Expenses is zero for $region, $skjema, $kontoklasse, $funksjon, $art, $belop") {
-                verifyValidationResult(
-                    validationReportEntries = sut.validate(kostraRecordList),
-                    expectError = expectError,
-                    expectedSeverity = Severity.ERROR,
-                    "Korrigér slik at fila inneholder utgiftsposteringene ($belop) i driftsregnskapet"
-                )
-            }
-        }
+    include(
+        validationRuleTest(
+            sut = Rule100SummeringDriftUtgiftsposteringer(),
+            forAllRows = listOf(
+                ForAllRowItem(
+                    "all conditions match",
+                    kostraRecordsInTest("420400", 1, 100, 590, 0),
+                    expectedErrorMessage = "Korrigér slik at fila inneholder utgiftsposteringene " +
+                            "(0) i driftsregnskapet"
+                ),
+                ForAllRowItem(
+                    "isOsloBydel() = true",
+                    kostraRecordsInTest("030114", 1, 100, 590, 0)
+                ),
+                ForAllRowItem(
+                    "isBevilgningDriftRegnskap = false",
+                    kostraRecordsInTest("420400", 0, 100, 590, 0)
+                ),
+                ForAllRowItem(
+                    "isUtgift = false",
+                    kostraRecordsInTest("420400", 1, 100, 600, 0),
+                    expectedErrorMessage = "Korrigér slik at fila inneholder utgiftsposteringene " +
+                            "(0) i driftsregnskapet"
+                ),
+                ForAllRowItem(
+                    "belop > 0",
+                    kostraRecordsInTest("420400", 1, 100, 590, 1)
+                ),
+            ),
+            expectedSeverity = Severity.ERROR,
+            useArguments = false
+        )
+    )
+}) {
+    companion object {
+        fun kostraRecordsInTest(
+            region: String,
+            kontoklasse: Int,
+            funksjon: Int,
+            art: Int,
+            belop: Int
+        ) = mapOf(
+            FIELD_REGION to region,
+            FIELD_SKJEMA to "0A",
+            FIELD_KONTOKLASSE to "$kontoklasse",
+            FIELD_FUNKSJON to "$funksjon",
+            FIELD_ART to "$art",
+            FIELD_BELOP to "$belop"
+        ).toKostraRecord().asList()
     }
-})
+}

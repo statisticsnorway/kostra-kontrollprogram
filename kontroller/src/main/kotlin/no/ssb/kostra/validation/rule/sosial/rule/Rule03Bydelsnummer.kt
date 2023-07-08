00fbb9c22804f7aaff03bec59eb6a1b3fd0b1e28
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-07-08 12:57:30 +0200

    refactored out oslo districts

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/sosial/rule/Rule03Bydelsnummer.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/sosial/rule/Rule03Bydelsnummer.kt
@@ -1,5 +1,6 @@
 package no.ssb.kostra.validation.rule.sosial.rule
 
+import no.ssb.kostra.SharedConstants.OSLO_DISTRICTS
 import no.ssb.kostra.SharedConstants.OSLO_MUNICIPALITY_ID
 import no.ssb.kostra.area.sosial.kvalifisering.KvalifiseringColumnNames.BYDELSNR_COL_NAME
 import no.ssb.kostra.area.sosial.kvalifisering.KvalifiseringColumnNames.KOMMUNE_NR_COL_NAME
@@ -13,19 +14,17 @@ class Rule03Bydelsnummer : AbstractRule<KostraRecord>(
     SosialRuleId.BYDELSNUMMER_03.title,
     Severity.ERROR
 ) {
-    private val osloDistrictIds = (1..15).map { it.toString().padStart(2, '0') }
-
     override fun validate(context: KostraRecord, arguments: KotlinArguments) = context.let { kostraRecord ->
         kostraRecord[KOMMUNE_NR_COL_NAME] to kostraRecord[BYDELSNR_COL_NAME]
     }.takeUnless { (municipalityId, districtId) ->
         when (municipalityId) {
-            OSLO_MUNICIPALITY_ID -> districtId in osloDistrictIds
+            OSLO_MUNICIPALITY_ID -> districtId in OSLO_DISTRICTS
             else -> districtId.isBlank()
         }
     }?.let { (municipalityId, districtId) ->
         createSingleReportEntryList(
             when (municipalityId) {
-                OSLO_MUNICIPALITY_ID -> "Korrigér bydel. Fant ${districtId}, forventet en av ${osloDistrictIds}."
+                OSLO_MUNICIPALITY_ID -> "Korrigér bydel. Fant ${districtId}, forventet en av ${OSLO_DISTRICTS}."
                 else -> "Korrigér bydel. Fant ${districtId}, forventet blank / '  '."
             }
         )

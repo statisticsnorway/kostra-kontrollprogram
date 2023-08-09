b68a6b709f3e268be199f84ad2873bcd183b741e
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-08-09 09:47:00 +0200

    Update Rule210InterneOverforingerKjopOgSalgTest.kt

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kirkekostra/Rule210InterneOverforingerKjopOgSalgTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/kirkekostra/Rule210InterneOverforingerKjopOgSalgTest.kt
@@ -30,10 +30,18 @@ class Rule210InterneOverforingerKjopOgSalgTest : BehaviorSpec({
                 ), false
             ),
             row(
-                "riktig regnskap og art, men sum internKjop + internSalg er utenfor interval på +-30",
+                "riktig regnskap og art, men sum internKjop + internSalg er utenfor interval på -30",
                 listOf(
                     kostraRecordInTest("0F", "380", 0),
                     kostraRecordInTest("0F", "780", -1000)
+                ),
+                true
+            ),
+            row(
+                "riktig regnskap og art, men sum internKjop + internSalg er utenfor interval på +30",
+                listOf(
+                    kostraRecordInTest("0F", "380", 1000),
+                    kostraRecordInTest("0F", "780", 0)
                 ),
                 true
             ),

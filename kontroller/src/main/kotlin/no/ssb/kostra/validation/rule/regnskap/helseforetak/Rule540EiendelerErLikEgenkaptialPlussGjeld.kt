883e64e3e940fc4a9cc314f84e8e638a921d0813
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-08-09 14:13:20 +0200

    Improve coverage Regnskap Helseforetak rule 540

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/regnskap/helseforetak/Rule540EiendelerErLikEgenkaptialPlussGjeld.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/regnskap/helseforetak/Rule540EiendelerErLikEgenkaptialPlussGjeld.kt
@@ -3,8 +3,8 @@ package no.ssb.kostra.validation.rule.regnskap.helseforetak
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_ART
 import no.ssb.kostra.area.regnskap.RegnskapConstants.FIELD_BELOP
 import no.ssb.kostra.program.KostraRecord
+import no.ssb.kostra.program.KotlinArguments
 import no.ssb.kostra.validation.report.Severity
-import no.ssb.kostra.validation.report.ValidationReportEntry
 import no.ssb.kostra.validation.rule.AbstractRule
 import no.ssb.kostra.validation.rule.regnskap.kostra.extensions.isBalanseRegnskap
 
@@ -12,41 +12,32 @@ class Rule540EiendelerErLikEgenkaptialPlussGjeld : AbstractRule<List<KostraRecor
     "Kontroll 540 : Eiendeler = egenkapital + gjeld",
     Severity.WARNING
 ) {
-    override fun validate(context: List<KostraRecord>): List<ValidationReportEntry>? {
-        val balanseRegnskap = context.filter { it.isBalanseRegnskap() }
+    override fun validate(context: List<KostraRecord>, arguments: KotlinArguments) = context
+        .filter { it.isBalanseRegnskap() }
+        .takeIf { it.any() }
+        ?.filter {
+            it.fieldAsIntOrDefault(FIELD_ART) in 100..195
+                    || it.fieldAsIntOrDefault(FIELD_ART) in 200..299
+        }
+        ?.partition {
+            it.fieldAsIntOrDefault(FIELD_ART) in 100..195
+        }
+        ?.let { (eiendelerPosteringer, egenkapitalOgGjeldPosteringer) ->
+            val egenkapitalOgGjeldPartionedPosteringer =
+                egenkapitalOgGjeldPosteringer.partition { it.fieldAsIntOrDefault(FIELD_ART) in 200..209 }
 
-        if (balanseRegnskap.isEmpty()) return null
-
-        // 1) Balanse må ha føring på eiendelskontiene , dvs. være høyere enn 0
-        val sumEiendeler: Int = balanseRegnskap
-            .filter { kostraRecord ->
-                kostraRecord.fieldAsIntOrDefault(FIELD_ART) in 100..195
-            }.sumOf { kostraRecord ->
-                kostraRecord.fieldAsIntOrDefault(FIELD_BELOP)
-            }
-
-        // 2) Balanse må ha føring på egenkapitalskontoer og/eller gjeldskontoer, dvs. være mindre enn 0
-        val sumEgenkapital: Int = balanseRegnskap
-            .filter { kostraRecord ->
-                kostraRecord.fieldAsIntOrDefault(FIELD_ART) in 200..209
-            }.sumOf { kostraRecord ->
-                kostraRecord.fieldAsIntOrDefault(FIELD_BELOP)
-            }
-
-        val sumGjeld = balanseRegnskap
-            .filter { kostraRecord ->
-                kostraRecord.fieldAsIntOrDefault(FIELD_ART) in 210..299
-            }.sumOf { kostraRecord ->
-                kostraRecord.fieldAsIntOrDefault(FIELD_BELOP)
-            }
-
-        // 3) sumEiendeler skal være lik Egenkapital + Gjeld. Differanser opptil +50' godtas, og skal ikke utlistes.
-        val sumBalanse = sumEiendeler + (sumEgenkapital + sumGjeld)
-
-        if (sumBalanse in -50..50) return null
-
-        return createSingleReportEntryList(
-            messageText = "Balansen ($sumBalanse) skal balansere ved at sum eiendeler ($sumEiendeler)  = sum egenkapital ($sumEgenkapital) + sum gjeld ($sumGjeld) . Differanser +/- 50' kroner godtas"
-        )
-    }
+            return@let Triple(
+                eiendelerPosteringer.sumOf { it.fieldAsIntOrDefault(FIELD_BELOP) },
+                egenkapitalOgGjeldPartionedPosteringer.first.sumOf { it.fieldAsIntOrDefault(FIELD_BELOP) },
+                egenkapitalOgGjeldPartionedPosteringer.second.sumOf { it.fieldAsIntOrDefault(FIELD_BELOP) }
+            )
+        }?.takeUnless { (sumEiendeler, sumEgenkapital, sumGjeld) ->
+            (sumEiendeler + sumEgenkapital + sumGjeld) in -50..50
+        }?.let { (sumEiendeler, sumEgenkapital, sumGjeld) ->
+            val sumBalanse = sumEiendeler + (sumEgenkapital + sumGjeld)
+            createSingleReportEntryList(
+                messageText = "Balansen ($sumBalanse) skal balansere ved at sum eiendeler ($sumEiendeler)  = sum " +
+                        "egenkapital ($sumEgenkapital) + sum gjeld ($sumGjeld) . Differanser +/- 50' kroner godtas"
+            )
+        }
 }
--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/helseforetak/Rule540EiendelerErLikEgenkaptialPlussGjeldTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/helseforetak/Rule540EiendelerErLikEgenkaptialPlussGjeldTest.kt
@@ -1,61 +1,64 @@
 package no.ssb.kostra.validation.rule.regnskap.helseforetak
 
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.data.forAll
-import io.kotest.data.row
 import no.ssb.kostra.area.regnskap.RegnskapConstants
-import no.ssb.kostra.area.regnskap.RegnskapFieldDefinitions.fieldDefinitions
-import no.ssb.kostra.program.extension.toKostraRecords
 import no.ssb.kostra.validation.report.Severity
-import no.ssb.kostra.validation.rule.TestUtils
+import no.ssb.kostra.validation.rule.ForAllRowItem
+import no.ssb.kostra.validation.rule.KostraTestFactory
+import no.ssb.kostra.validation.rule.regnskap.RegnskapTestUtils
 
 class Rule540EiendelerErLikEgenkaptialPlussGjeldTest : BehaviorSpec({
-    val sut = Rule540EiendelerErLikEgenkaptialPlussGjeld()
+    include(
+        KostraTestFactory.validationRuleTest(
+            sut = Rule540EiendelerErLikEgenkaptialPlussGjeld(),
+            forAllRows = listOf(
+                ForAllRowItem(
+                    "feil skjema",
+                    listOf(
+                        kostraRecordInTest("0X", "100", 0),
+                    ),
+                ),
+                ForAllRowItem(
+                    "riktig skjema, men feil sektor",
+                    listOf(
+                        kostraRecordInTest("0Y", "999", 0),
+                    ),
+                ),
+                ForAllRowItem(
+                    "riktig skjema og sektor, men feil beløp",
+                    listOf(
+                        kostraRecordInTest("0Y", "100", 1000),
+                        kostraRecordInTest("0Y", "200", 0),
+                        kostraRecordInTest("0Y", "210", 0),
+                    ),
+                    expectedErrorMessage = "Balansen (1000) skal balansere ved at sum eiendeler (1000)  = sum " +
+                            "egenkapital (0) + sum gjeld (0) . Differanser +/- 50' kroner godtas"
+                ),
+                ForAllRowItem(
+                    "riktig skjema, sektor og beløp, differanse = 0",
+                    listOf(
+                        kostraRecordInTest("0Y", "100", 1000),
+                        kostraRecordInTest("0Y", "200", -500),
+                        kostraRecordInTest("0Y", "210", -500),
+                    ),
+                ),
+            ),
+            expectedSeverity = Severity.WARNING
+        )
+    )
 
-    Given("context") {
-        forAll(
-            row("0X", "0", "0", "-500", false), // skjema mismatch -> OK
-            row("0Y", "0", "0", "0", false), // belop match -> OK
-            row("0Y", "1000", "0", "0", true), // belop mismatch -> Fail
-            row("0Y", "0", "-500", "0", true), // belop mismatch -> Fail
-            row("0Y", "0", "0", "-500", true), // belop mismatch -> Fail
-            row("0Y", "1000", "-500", "0", true), // belop mismatch -> Fail
-            row("0Y", "1000", "0", "-500", true), // belop mismatch -> Fail
-            row("0Y", "0", "-500", "-500", true), // belop mismatch -> Fail
-            row("0Y", "1000", "-500", "-500", false), // belop match -> OK
-        ) { skjema, sumEiendeler, sumEgenkapital, sumGjeld, expectError ->
-            val kostraRecordList = listOf(
-                mapOf(
-                    RegnskapConstants.FIELD_SKJEMA to skjema,
-                    RegnskapConstants.FIELD_ART to "100",
-                    RegnskapConstants.FIELD_BELOP to sumEiendeler
-                ),
-                mapOf(
-                    RegnskapConstants.FIELD_SKJEMA to skjema,
-                    RegnskapConstants.FIELD_ART to "200",
-                    RegnskapConstants.FIELD_BELOP to sumEgenkapital
-                ),
-                mapOf(
-                    RegnskapConstants.FIELD_SKJEMA to skjema,
-                    RegnskapConstants.FIELD_ART to "210",
-                    RegnskapConstants.FIELD_BELOP to sumGjeld
-                ),
-            ).toKostraRecords(fieldDefinitions)
-            When("$skjema, $sumEiendeler, $sumEgenkapital, $sumGjeld") {
-                val sumBalanse = sumEiendeler.toInt() + (sumEgenkapital.toInt() + sumGjeld.toInt())
-                TestUtils.verifyValidationResult(
-                    validationReportEntries = sut.validate(kostraRecordList),
-                    expectError = expectError,
-                    expectedSeverity = Severity.WARNING,
-                    "Balansen ($sumBalanse) skal balansere ved at sum eiendeler ($sumEiendeler)  = sum egenkapital ($sumEgenkapital) + sum gjeld ($sumGjeld) . Differanser +/- 50' kroner godtas"
-                )
-            }
-        }
+}) {
+    companion object {
+        private fun kostraRecordInTest(
+            skjema: String,
+            sektor: String,
+            belop: Int
+        ) = RegnskapTestUtils.regnskapRecordInTest(
+            mapOf(
+                RegnskapConstants.FIELD_SKJEMA to skjema,
+                RegnskapConstants.FIELD_SEKTOR to sektor,
+                RegnskapConstants.FIELD_BELOP to belop.toString()
+            )
+        )
     }
-})
-/*
-* "Balansen (-1000) skal balansere ved at sum eiendeler (0)  = sum egenkapital (-1000) + sum gjeld (0) . Differanser +/- 50' kroner godtas" should start with
-* "Balansen (0-10000) skal balansere ved at sum eiendeler (0)  = sum egenkapital (-1000) + sum gjeld (0) . Differanser +/- 50' kroner godtas" (diverged at index 10)
-
-*
-* */
+}

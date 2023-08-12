3a4b7f3bc856cb36c4539cef90790323c2e8020e
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-08-12 20:05:42 +0200

    Update KommuneKostraMain.kt

--- kontroller/src/main/kotlin/no/ssb/kostra/area/regnskap/kostra/KommuneKostraMain.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/area/regnskap/kostra/KommuneKostraMain.kt
@@ -228,13 +228,7 @@ class KommuneKostraMain(
     )
 
     private fun getArtAsList(): List<String> =
-        if (RegnskapConstants.getRegnskapTypeBySkjema(arguments.skjema).any {
-                it in listOf(
-                    ACCOUNTING_TYPE_BEVILGNING,
-                    ACCOUNTING_TYPE_REGIONALE
-                )
-            }
-        ) {
+        if (arguments.skjema in listOf("0A", "0C", "0I", "0K", "0M", "0P")) {
             val result = ArrayList<String>(basisArter)
             when (arguments.skjema) {
                 "0A", "0M" -> {
@@ -260,27 +254,23 @@ class KommuneKostraMain(
         } else
             emptyList()
 
-    private fun getSektorAsList(): List<String> {
-        if (RegnskapConstants.getRegnskapTypeBySkjema(arguments.skjema).none {
-                it == ACCOUNTING_TYPE_BALANSE
-            }
-        )
-            return emptyList()
-
+    private val sektorList =
+        if (arguments.skjema in listOf("0B", "0D", "0J", "0L", "0N", "0Q"))
         // Sektorer
-        return listOf(
-            // @formatter:off
-            "000", "070", "080",
-            "110", "151", "152",
-            "200", "320",
-            "355", "395",
-            "430", "450", "499",
-            "550", "570",
-            "610", "640", "650",
-            "890", "900"
-            // @formatter:on
-        )
-    }
+            listOf(
+                // @formatter:off
+                "000", "070", "080",
+                "110", "151", "152",
+                "200", "320",
+                "355", "395",
+                "430", "450", "499",
+                "550", "570",
+                "610", "640", "650",
+                "890", "900"
+                // @formatter:on
+            )
+        else emptyList()
+
 
     // Kun gyldig i investering og skal fjernes fra drift
     private fun getInvalidDriftArtList() =
@@ -322,7 +312,7 @@ class KommuneKostraMain(
         Rule010Funksjon(funksjonList = getFunksjonAsList()),
         Rule011Kapittel(kapittelList = getKapittelAsList()),
         Rule012Art(artList = getArtAsList()),
-        Rule013Sektor(sektorList = getSektorAsList()),
+        Rule013Sektor(sektorList = sektorList),
         Rule014Belop(),
         Rule015Duplicates(mappingDuplicates(arguments = arguments)),
         Rule020KombinasjonDriftKontoklasseFunksjon(invalidDriftFunksjonList = getInvalidDriftFunksjonList()),

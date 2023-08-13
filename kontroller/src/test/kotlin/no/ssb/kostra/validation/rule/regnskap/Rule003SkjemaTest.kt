8d6ba40b625f8cab8361ee02b885d2bc0aab41ba
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-08-13 14:34:04 +0200

    Update Rule 003 for Kvartal

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/Rule003SkjemaTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/regnskap/Rule003SkjemaTest.kt
@@ -34,4 +34,31 @@ class Rule003SkjemaTest : BehaviorSpec({
             }
         }
     }
+
+    Given("kvartal") {
+        val sut = Rule003Skjema()
+        val fieldDefinitions = listOf(FieldDefinition(name = FIELD_SKJEMA, from = 1, to = 2))
+        val arguments = KotlinArguments(skjema = "OAK1", aargang = "2023", region = "1234")
+
+        When("valid list of KostraRecord") {
+            val kostraRecordList = listOf(
+                "OA".toKostraRecord(1, fieldDefinitions)
+            )
+
+            Then("validation should pass with no errors") {
+                sut.validate(kostraRecordList, arguments).shouldBeNull()
+            }
+        }
+
+        When("invalid list of KostraRecord") {
+            val kostraRecordList = listOf(
+                "XX".toKostraRecord(1, fieldDefinitions)
+            )
+
+            Then("validation should result in errors") {
+                sut.validate(kostraRecordList, arguments).shouldNotBeNull()
+            }
+        }
+    }
+
 })

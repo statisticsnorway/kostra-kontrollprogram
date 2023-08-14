0c895baa1237acd6703f1b5812a583c9ed32c05f
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-08-14 09:22:58 +0200

    Reinstated **/KostraRecordExtensionsGenerics.kt to sonar.exclusions, reformatted code in KostraRecordExtensionsGenerics.kt

--- kontroller/src/main/kotlin/no/ssb/kostra/program/extension/KostraRecordExtensionsGenerics.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/program/extension/KostraRecordExtensionsGenerics.kt
@@ -4,7 +4,10 @@ import no.ssb.kostra.program.KostraRecord
 import java.time.LocalDate
 import kotlin.reflect.typeOf
 
-inline fun <reified T : Any?> KostraRecord.fieldAs(field: String, trim: Boolean = true): T = typeOf<T>().run {
+inline fun <reified T : Any?> KostraRecord.fieldAs(
+    field: String,
+    trim: Boolean = true
+): T = typeOf<T>().run {
     when (classifier) {
         Int::class -> if (isMarkedNullable) fieldAsInt(field) else fieldAsIntOrDefault(field)
         LocalDate::class -> if (isMarkedNullable) fieldAsLocalDate(field) else fieldAsLocalDate(field)!!

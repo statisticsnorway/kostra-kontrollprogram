164d266d6264091952af2ed705c1396847cfa2f3
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-08-18 14:59:45 +0200

    Test is now a MicronautTest

--- konsoll/src/test/kotlin/no/ssb/kostra/program/KostraKontrollprogramKonsollCommandTest.kt
+++ konsoll/src/test/kotlin/no/ssb/kostra/program/KostraKontrollprogramKonsollCommandTest.kt
@@ -5,30 +5,34 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
-class KostraKontrollprogramCommandSpec: BehaviorSpec({
+@MicronautTest(environments = [Environment.CLI, Environment.TEST])
+class KostraKontrollprogramCommandSpec(
+    applicationContext: ApplicationContext
+) : BehaviorSpec({
 
     Given("kostra-kontrollprogram-konsoll") {
-        val ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
-
         When("invocation with -v") {
             val baos = ByteArrayOutputStream()
+            val origStream = System.out
+
             System.setOut(PrintStream(baos))
 
             val args = arrayOf("-v")
-            PicocliRunner.run(KostraKontrollprogramCommand::class.java, ctx, *args)
+            PicocliRunner.run(KostraKontrollprogramCommand::class.java, applicationContext, *args)
 
             Then("should display greeting") {
                 baos.toString() shouldContain "Hi!"
             }
+
+            System.setOut(origStream)
         }
 
         When("other") {
             // add more tests...
         }
-
-        ctx.close()
     }
 })

0d78410b3d0433f220e62e9944f341118476f904
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-08-19 20:38:11 +0200

    Removed residues of application-common

--- kontroller/src/main/kotlin/no/ssb/kostra/felles/git/GitProperties.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/felles/git/GitProperties.kt
@@ -1,6 +1,5 @@
 package no.ssb.kostra.felles.git
 
-// TODO: Move this to application-common when ErrorReport is rewritten
 data class GitProperties(
     val tags: String
 )
--- kontroller/src/main/kotlin/no/ssb/kostra/felles/git/GitPropertyLoader.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/felles/git/GitPropertyLoader.kt
@@ -2,7 +2,6 @@ package no.ssb.kostra.felles.git
 
 import java.util.Properties
 
-// TODO: Move this to application-common when ErrorReport is rewritten
 object GitPropertiesLoader {
     const val DEFAULT_GIT_PROPERTIES_FILENAME = "/git.properties"
     const val NOT_AVAILABLE_VALUE = "N/A"

05515c3e103510e650b56f8af7317e3b64635099
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-08-21 15:25:05 +0200

    Fix broken test

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/report/ValidationReportTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/report/ValidationReportTest.kt
@@ -10,45 +10,47 @@ import java.time.LocalDateTime
 class ValidationReportTest : BehaviorSpec({
     Given("ValidationReport only a minimum of arguments to pass the test") {
         forAll(
-            row("", Severity.FATAL, )
-        ){ description ->
-
-
-        }
-
-        val validationReportArguments = ValidationReportArguments(
-            kotlinArguments = KotlinArguments(
-                skjema = "0A",
-                aargang = "2023",
-                region = "1234",
-                startTime = LocalDateTime.of(2022, 1, 1, 1, 1, 0),
-            ),
-            validationResult = ValidationResult(
-                reportEntries = listOf(
-                    ValidationReportEntry(
-                        severity = Severity.ERROR,
-                        ruleName = "Kontroll Test 1",
-                        messageText = "Feilmelding",
-                        lineNumbers = listOf(1)
-                    )
+            row(Severity.FATAL),
+            row(Severity.ERROR),
+            row(Severity.WARNING),
+            row(Severity.INFO),
+            row(Severity.OK),
+        ) { severity ->
+            val validationReportArguments = ValidationReportArguments(
+                kotlinArguments = KotlinArguments(
+                    skjema = "0A",
+                    aargang = "2023",
+                    region = "1234",
+                    startTime = LocalDateTime.of(2022, 1, 1, 1, 1, 0),
                 ),
-                numberOfControls = 1,
-                endTime = LocalDateTime.of(2022, 1, 1, 1, 1, 1),
+                validationResult = ValidationResult(
+                    reportEntries = listOf(
+                        ValidationReportEntry(
+                            severity = severity,
+                            ruleName = "Kontroll Test 1",
+                            messageText = "Feilmelding",
+                            lineNumbers = listOf(1)
+                        )
+                    ),
+                    numberOfControls = 1,
+                    endTime = LocalDateTime.of(2022, 1, 1, 1, 1, 1),
+                )
             )
-        )
-        val sut = ValidationReport(validationReportArguments = validationReportArguments)
+            val sut = ValidationReport(validationReportArguments = validationReportArguments)
 
-        When("count is called") {
-            val result = sut.toString()
+            When("count is called") {
+                val result = sut.toString()
 
-            Then("result should be as expected") {
-                result shouldContain "0A"
-                result shouldContain "2023"
-                result shouldContain "1234"
-                result shouldContain "Kontroll Test 1"
-                result shouldContain "LOCAL-SNAPSHOT"
+                Then("result should be as expected") {
+                    result shouldContain "0A"
+                    result shouldContain "2023"
+                    result shouldContain "1234"
+                    result shouldContain "Kontroll Test 1"
+                    result shouldContain "LOCAL-SNAPSHOT"
 
+                }
             }
         }
     }
+
 })

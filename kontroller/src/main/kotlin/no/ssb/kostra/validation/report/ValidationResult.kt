41167dbb39f8e84593efa2b691a711fb2e58fd67
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-08-22 09:50:26 +0200

    Refactored ValidationResult, attempt #2

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/report/ValidationResult.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/report/ValidationResult.kt
@@ -3,18 +3,17 @@ package no.ssb.kostra.validation.report
 import java.time.LocalDateTime
 
 
+private fun List<ValidationReportEntry>.severity(): Severity =
+    if (this.isEmpty()) Severity.OK
+    else this.maxOf { it.severity }
+
 data class ValidationResult(
     val reportEntries: List<ValidationReportEntry>,
     val numberOfControls: Int,
     val statsReportEntries: List<StatsReportEntry> = emptyList(),
     val endTime: LocalDateTime = LocalDateTime.now(),
 ) {
-    val severity: Severity = when {
-        numberOfControls == 0 -> Severity.ERROR
-        reportEntries.isNotEmpty() -> reportEntries.maxOf { it.severity }
-        else -> Severity.OK
-    }
-
+    val severity: Severity = if (numberOfControls == 0) Severity.ERROR else reportEntries.severity()
     val count: Int = reportEntries.size
 
     val uniqueReportEntries: List<ValidationReportEntry>

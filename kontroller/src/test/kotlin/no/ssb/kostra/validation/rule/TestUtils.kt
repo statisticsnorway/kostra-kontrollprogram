5479e98cfc4e8a10795a3e15c45c64363ceb6db1
oho <joachim.h@outlook.com>
2023-08-30 14:59:10 +0200

    Made changes to validationRuleTest and ForAllRowItem to allow checking multiple error messages

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/TestUtils.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/TestUtils.kt
@@ -1,6 +1,6 @@
 package no.ssb.kostra.validation.rule
 
-import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.asClue
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
@@ -11,19 +11,59 @@ import no.ssb.kostra.validation.report.ValidationReportEntry
 object TestUtils {
     fun verifyValidationResult(
         validationReportEntries: Collection<ValidationReportEntry>?,
-        expectError: Boolean,
+        numberOfExpectedErrors: Int,
         expectedSeverity: Severity,
         expectedErrorText: String,
-        expectedSize: Int = 1
+        expectedContextId: String? = null,
     ) {
-        if (expectError) {
-            validationReportEntries.shouldNotBeNull()
-            validationReportEntries.size.shouldBe(expectedSize)
+        if (numberOfExpectedErrors > 0) {
 
-            assertSoftly(validationReportEntries.first()) {
-                severity.shouldBe(expectedSeverity)
-                messageText.shouldStartWith(expectedErrorText)
+            "At least one error should be present when expected".asClue {
+                validationReportEntries.shouldNotBeNull()
+
+                "Number of actual errors should match the number of expected errors".asClue {
+                    validationReportEntries.size.shouldBe(numberOfExpectedErrors)
+                }
+
+                ("There should be an entry with (contextId=${expectedContextId}) and message which starts with " +
+                    "(\"${expectedErrorText}\") among the following entries: \n($validationReportEntries)").asClue {
+
+                    val validationReportEntry = validationReportEntries.find {
+                        it.contextId == expectedContextId
+                            && it.messageText.startsWith(expectedErrorText)
+                    }
+
+                    validationReportEntry.shouldNotBeNull()
+
+                    validationReportEntry.asClue {
+                        it.severity.shouldBe(expectedSeverity)
+                        it.contextId.shouldBe(expectedContextId)
+                        it.messageText.shouldStartWith(expectedErrorText)
+                    }
+                }
             }
-        } else validationReportEntries.shouldBeNull()
+        } else {
+            { "No error should be present when not expected" }.asClue {
+                validationReportEntries.shouldBeNull()
+            }
+        }
+    }
+
+    fun verifyValidationResult(
+        validationReportEntries: Collection<ValidationReportEntry>?,
+        expectError: Boolean,
+        expectedSeverity: Severity,
+        expectedErrorText: String
+    ) {
+        verifyValidationResult(
+            validationReportEntries,
+            numberOfExpectedErrors = when (expectError) {
+                false -> 0
+                true -> 1
+            },
+            expectedSeverity = expectedSeverity,
+            expectedErrorText = expectedErrorText,
+            expectedContextId = ""
+        )
     }
 }

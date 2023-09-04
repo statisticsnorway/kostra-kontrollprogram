0bc9c63fab3af038f2bb1a1cc862c0f706d584e8
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-09-04 21:50:04 +0200

    Revert "Removed redundant default value"
    This reverts commit 7e85cb810b36259f7916148be179998d123892d1.

--- web/src/main/kotlin/no/ssb/kostra/web/viewmodel/CompanyIdVm.kt
+++ web/src/main/kotlin/no/ssb/kostra/web/viewmodel/CompanyIdVm.kt
@@ -7,5 +7,5 @@ import jakarta.validation.constraints.Pattern
 data class CompanyIdVm(
 
     @field:Pattern(regexp = "\\d{9}", message = "Må starte med 8 eller 9 etterfulgt av 8 siffer")
-    val orgnr: String
+    val orgnr: String = ""
 )

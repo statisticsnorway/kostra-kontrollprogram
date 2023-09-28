8f396efee0815edc0a612308dc2d65829890b311
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2023-09-28 12:19:19 +0200

    Clarify messageText

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/regnskap/Rule015Duplicates.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/regnskap/Rule015Duplicates.kt
@@ -9,13 +9,17 @@ class Rule015Duplicates(
 ) : AbstractNoArgsRule<List<KostraRecord>>("Kontroll 015 : Dubletter", Severity.WARNING) {
     override fun validate(context: List<KostraRecord>) = context
         .groupBy { kostraRecord ->
-            fieldNameTitlePairList.first.joinToString(" * ") { fieldName -> kostraRecord[fieldName].trim() }
-        }.filter { (_, group) -> group.size > 1 }
+            fieldNameTitlePairList.first
+                .zip(fieldNameTitlePairList.second)
+                .joinToString(" * ") { (fieldName, title) ->
+                    "$title = '${kostraRecord[fieldName].trim()}'"
+                }
+        }
+        .filter { (_, group) -> group.size > 1 }
         .takeIf { it.any() }
         ?.flatMap { (key, group) ->
             createSingleReportEntryList(
-                messageText = """Det er oppgitt flere beløp på samme kombinasjon av 
-                    (${fieldNameTitlePairList.second.joinToString(" * ")} - $key).<br/>
+                messageText = """Det er oppgitt flere beløp på samme kombinasjon av ($key).<br/>
                  Hvis dette er riktig, kan du sende inn filen, og beløpene summeres hos SSB. 
                  Dersom dette er feil må recordene korrigeres før innsending til SSB. 
                 """.trimIndent(),

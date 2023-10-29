8186840d51cf6a4ce6e220bc26d348ebda01c3a9
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-10-29 16:13:14 +0100

    Only FileReportEntryVm instances without line numbers should be reduced

--- web/src/test/kotlin/no/ssb/kostra/web/extension/MappingFromConsoleAppExtensionsKtTest.kt
+++ web/src/test/kotlin/no/ssb/kostra/web/extension/MappingFromConsoleAppExtensionsKtTest.kt
@@ -2,17 +2,83 @@ package no.ssb.kostra.web.extension
 
 import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.shouldBe
 import no.ssb.kostra.program.KotlinArguments
 import no.ssb.kostra.validation.report.Severity
 import no.ssb.kostra.validation.report.ValidationReportArguments
 import no.ssb.kostra.validation.report.ValidationReportEntry
 import no.ssb.kostra.validation.report.ValidationResult
+import no.ssb.kostra.web.extensions.reduceReportEntries
 import no.ssb.kostra.web.extensions.toErrorReportVm
 import no.ssb.kostra.web.viewmodel.CompanyIdVm
+import no.ssb.kostra.web.viewmodel.FileReportEntryVm
 import java.time.LocalDateTime
 
 class MappingFromConsoleAppExtensionsKtTest : BehaviorSpec({
+
+    Given("List<FileReportEntryVm>.reduceReportEntries") {
+        val firstValidationReportEntry = FileReportEntryVm(
+            severity = Severity.ERROR,
+            caseworker = "caseworker",
+            journalId = "journalId",
+            individId = "individId",
+            contextId = "contextId",
+            ruleName = "ruleName",
+            messageText = "messageText",
+            lineNumbers = listOf(1, 2, 3)
+        )
+
+        val secondValidationReportEntry = firstValidationReportEntry.copy(
+            caseworker = "caseworker2",
+            journalId = "journalId2",
+            individId = "individId2",
+            contextId = "contextId2",
+            lineNumbers = listOf(4, 5, 6)
+        )
+
+        val thirdValidationReportEntry = firstValidationReportEntry.copy(
+            caseworker = "caseworker3",
+            journalId = "journalId3",
+            individId = "individId3",
+            contextId = "contextId3",
+            lineNumbers = emptyList()
+        )
+
+        When("reduceReportEntries") {
+            val reducedList = listOf(
+                firstValidationReportEntry,
+                secondValidationReportEntry,
+                thirdValidationReportEntry
+            ).reduceReportEntries()
+
+            Then("reducedList should be as expected") {
+                reducedList.size shouldBe 2
+                assertSoftly(reducedList.first()) {
+                    severity shouldBe firstValidationReportEntry.severity
+                    caseworker shouldBe firstValidationReportEntry.caseworker
+                    journalId shouldBe firstValidationReportEntry.journalId
+                    individId shouldBe firstValidationReportEntry.individId
+                    contextId shouldBe firstValidationReportEntry.contextId
+                    ruleName shouldBe firstValidationReportEntry.ruleName
+                    messageText shouldBe firstValidationReportEntry.messageText
+                    lineNumbers shouldBe
+                            firstValidationReportEntry.lineNumbers + secondValidationReportEntry.lineNumbers
+                }
+
+                assertSoftly(reducedList.last()) {
+                    severity shouldBe thirdValidationReportEntry.severity
+                    caseworker shouldBe thirdValidationReportEntry.caseworker
+                    journalId shouldBe thirdValidationReportEntry.journalId
+                    individId shouldBe thirdValidationReportEntry.individId
+                    contextId shouldBe thirdValidationReportEntry.contextId
+                    ruleName shouldBe thirdValidationReportEntry.ruleName
+                    messageText shouldBe thirdValidationReportEntry.messageText
+                    lineNumbers.shouldBeEmpty()
+                }
+            }
+        }
+    }
 
     Given("ValidationReportArguments.toErrorReportVm") {
         val kotlinArguments = KotlinArguments(

f48a8e628167f8e62d0b1f823fc059ad426bbeca
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-10-30 11:12:47 +0100

    Improved test

--- web/src/test/kotlin/no/ssb/kostra/web/config/UiConfigTest.kt
+++ web/src/test/kotlin/no/ssb/kostra/web/config/UiConfigTest.kt
@@ -1,9 +1,13 @@
 package no.ssb.kostra.web.config
 
+import io.kotest.assertions.asClue
 import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotBeBlank
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 
 @MicronautTest
@@ -13,17 +17,27 @@ class UiConfigTest(
 
     Given("UiConfig") {
         Then("should be as expected") {
-            sut.aarganger.size.shouldBeGreaterThan(1)
 
-            assertSoftly(sut.skjematyper.first { it.id == "0BK3" }) {
-                tittel shouldBe "0BK3. Balanseregnskap - 3. kvartal for kommuner"
-                kvartal shouldBe "3"
+            "aarganger should contain more than one element".asClue {
+                sut.aarganger.size.shouldBeGreaterThan(1)
             }
 
-            assertSoftly(sut.skjematyper.first { it.id == "0X" }) {
-                tittel shouldBe "0X. Resultatregnskap for helseforetak"
-                labelOrgnr shouldBe "Organisasjonsnummer for foretaket"
-                labelOrgnrVirksomhetene shouldBe "Organisasjonsnummer for virksomhetene"
+            "kvartal should not be null/empty/blank and should match title".asClue {
+                val itemsWithKvartal = sut.skjematyper.filter { it.tittel.contains("kvartal") }
+                itemsWithKvartal.shouldNotBeEmpty()
+
+                itemsWithKvartal.forEach {
+                    it.kvartal.shouldNotBeBlank()
+                    it.tittel.shouldContain("${it.kvartal}. kvartal")
+                }
+            }
+
+            "tittel, labelOrgnr and labelOrgnrVirksomhetene should be mapped".asClue {
+                assertSoftly(sut.skjematyper.first { it.id == "0X" }) {
+                    tittel shouldBe "0X. Resultatregnskap for helseforetak"
+                    labelOrgnr shouldBe "Organisasjonsnummer for foretaket"
+                    labelOrgnrVirksomhetene shouldBe "Organisasjonsnummer for virksomhetene"
+                }
             }
         }
     }

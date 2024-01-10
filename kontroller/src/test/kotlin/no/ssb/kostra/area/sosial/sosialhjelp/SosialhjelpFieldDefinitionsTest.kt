ce99bd843d2c9d804f81e4ff7423370914c54c1a
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-01-10 16:10:01 +0100

    Update SosialhjelpFieldDefinitionsTest.kt

--- kontroller/src/test/kotlin/no/ssb/kostra/area/sosial/sosialhjelp/SosialhjelpFieldDefinitionsTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/area/sosial/sosialhjelp/SosialhjelpFieldDefinitionsTest.kt
@@ -1,12 +1,7 @@
 package no.ssb.kostra.area.sosial.sosialhjelp
 
-import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.string.shouldStartWith
-import no.ssb.kostra.area.sosial.kvalifisering.KvalifiseringColumnNames
-import no.ssb.kostra.area.sosial.kvalifisering.KvalifiseringFieldDefinitions
-import no.ssb.kostra.program.extension.toKostraRecord
 
 class SosialhjelpFieldDefinitionsTest : BehaviorSpec({
 
@@ -16,30 +11,29 @@ class SosialhjelpFieldDefinitionsTest : BehaviorSpec({
         val sut = SosialhjelpFieldDefinitions.fieldDefinitions
 
         When("reading record strings") {
-            sut.forEachIndexed { index, fieldDefinition ->
+            sut.forEach { fieldDefinition ->
                 with(fieldDefinition) {
-                    println("- name: $name")
-                    println("  dataType: $dataType")
-                    println("  from: $from")
-                    println("  to: $to")
-                    println("  length: $size")
-                    println("  mandatory: $mandatory")
+                    println("  - name: $name")
+                    println("    dataType: $dataType")
+                    println("    from: $from")
+                    println("    size: $size")
+                    if (mandatory)
+                        println("    mandatory: $mandatory")
 
-                    if(dataType.toString() == "DATE_TYPE")
-                        println("  datePattern: $datePattern")
-                    if (codeList.isNotEmpty()){
-                        println("  codeList:")
+                    if (dataType.toString() == "DATE_TYPE")
+                        println("    datePattern: $datePattern")
+
+                    if (codeList.isNotEmpty()) {
+                        println("    codeList:")
                         codeList.forEach {
-                            println("  - code: ${it.code}")
-                            println("    value: ${it.value}")
+                            println("    - code: ${it.code}")
+                            println("      value: ${it.value}")
                         }
                     }
-
-                    println(fieldDefinition)
                 }
             }
             Then("records should be as expected") {
-                true shouldBe true
+                sut.size shouldBe 78
             }
         }
     }

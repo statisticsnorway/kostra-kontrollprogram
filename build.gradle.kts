5fdf7b0e4af6cc7f7cdc41e6e1d600c77603cf92
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-04-07 18:34:11 +0200

    Added SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,27 @@ group = "no.ssb.kostra"
 plugins {
     kotlin("jvm") version libs.versions.kotlin apply false
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("org.sonarqube") version "5.0.0.4638"
+    id("jacoco")
 }
 repositories { mavenCentral() }
+
+subprojects {
+    sonarqube {
+        properties {
+            property("sonar.organization", "statisticsnorway")
+            property("sonar.projectKey", "tatisticsnorway_kostra-kontrollprogram")
+            property("sonar.host.url", "https://sonarcloud.io")
+            property(
+                "sonar.exclusions", """
+                **/Application.kt,
+                **/node_modules/**/*,
+                **/KostraRecordExtensionsGenerics.kt,
+                **/KostraKontrollprogramCommand.kt,
+                **/MappingToConsoleAppExtensions.kt,
+                **/kostra/barnevern/**/*
+                """.trimIndent()
+            )
+        }
+    }
+}

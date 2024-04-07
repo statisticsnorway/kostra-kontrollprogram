7012467d6ccefbf9770095c0fe3ebeedbe379c79
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-04-07 02:58:08 +0200

    Removed obsolete task

--- konsoll/build.gradle.kts
+++ konsoll/build.gradle.kts
@@ -20,13 +20,3 @@ dependencies {
     testImplementation(libs.kotest.runner.junit5.jvm)
     testImplementation(libs.mockk.jvm)
 }
-
-tasks.jar {
-    manifest.attributes["Main-Class"] = "no.ssb.kostra.program.KostraKontrollprogramCommand"
-    val dependencies = configurations
-        .runtimeClasspath
-        .get()
-        .map { zipTree(it) }
-    from(dependencies)
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}

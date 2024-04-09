64c3ac6b98ead2ee2454961fc910819675021ef5
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-04-09 18:20:53 +0200

    Small simplification, attempt #2

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,10 +29,8 @@ sonarqube {
 subprojects {
     apply(plugin = "jacoco")
 
-    tasks.withType<JacocoReport>().configureEach {
+    tasks.withType<JacocoReport>().all {
         dependsOn(tasks.withType<Test>())
-        reports {
-            xml.required.set(true)
-        }
+        reports { xml.required = true }
     }
 }

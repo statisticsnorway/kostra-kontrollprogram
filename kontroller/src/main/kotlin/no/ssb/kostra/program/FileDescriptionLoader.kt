04b314c41be8f52b09c755769d6d13545cc19c2f
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-06-25 09:57:53 +0200

    Update FileDescriptionLoader.kt
    Refactor to use getResourceAsStream() to reach the resources in the jar file

--- kontroller/src/main/kotlin/no/ssb/kostra/program/FileDescriptionLoader.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/program/FileDescriptionLoader.kt
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 object FileDescriptionLoader {
     private val mapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
@@ -12,11 +15,11 @@ object FileDescriptionLoader {
         getResourceAsFileDescription(fileName).fields
 
 
-    fun getResourceAsFileDescription(fileName: String): FileDescription {
-        val fileDescription: FileDescription? = this::class.java.classLoader.getResource(fileName)
-            ?.let { mapper.readValue(it.readText()) }
+    fun getResourceAsFileDescription(fileName: String): FileDescription =
+        this::class.java.classLoader.getResourceAsStream(fileName)
+            ?.let { inputStream -> InputStreamReader(inputStream) }
+            ?.let { inputStreamReader -> BufferedReader(inputStreamReader) }
+            ?.let { bufferedReader -> mapper.readValue(bufferedReader.readText()) as FileDescription? }
+            ?: FileDescription()
 
-        return fileDescription ?: FileDescription()
-
-    }
 }

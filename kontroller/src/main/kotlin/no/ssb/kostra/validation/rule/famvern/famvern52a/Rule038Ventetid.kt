491588e33ce2972e4b6397c3e12baa5e21a5bb88
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-07-05 13:03:06 +0200

    Update Rule038Ventetid.kt

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/famvern/famvern52a/Rule038Ventetid.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/famvern/famvern52a/Rule038Ventetid.kt
@@ -1,8 +1,9 @@
 package no.ssb.kostra.validation.rule.famvern.famvern52a
 
-import no.ssb.kostra.area.famvern.famvern52a.Familievern52aColumnNames
 import no.ssb.kostra.area.famvern.famvern52a.Familievern52aColumnNames.FORSTE_SAMT_A_COL_NAME
 import no.ssb.kostra.area.famvern.famvern52a.Familievern52aColumnNames.HENV_DATO_A_COL_NAME
+import no.ssb.kostra.area.famvern.famvern52a.Familievern52aColumnNames.JOURNAL_NR_A_COL_NAME
+import no.ssb.kostra.area.famvern.famvern52a.Familievern52aColumnNames.KONTOR_NR_A_COL_NAME
 import no.ssb.kostra.program.KostraRecord
 import no.ssb.kostra.program.extension.fieldAs
 import no.ssb.kostra.validation.report.Severity
@@ -16,27 +17,29 @@ class Rule038Ventetid : AbstractNoArgsRule<List<KostraRecord>>(
 ) {
     override fun validate(context: List<KostraRecord>) = context
         .filter {
-            val henvendelsesDato = it.fieldAs<LocalDate?>(HENV_DATO_A_COL_NAME)
-            val samtaleDato = it.fieldAs<LocalDate?>(FORSTE_SAMT_A_COL_NAME)
             it[HENV_DATO_A_COL_NAME].isNotBlank()
                     && it[FORSTE_SAMT_A_COL_NAME].isNotBlank()
-                    && henvendelsesDato != null
-                    && samtaleDato != null
-                    && Period.between(henvendelsesDato, samtaleDato).years >= 1
-                    && (
-                    Period.between(henvendelsesDato, samtaleDato).months > 0
-                            || Period.between(henvendelsesDato, samtaleDato).days > 0
-                    )
+        }
+        .filter {
+            it.fieldAs<LocalDate?>(HENV_DATO_A_COL_NAME) != null
+                    && it.fieldAs<LocalDate?>(FORSTE_SAMT_A_COL_NAME) != null
+                    && it.fieldAs<LocalDate>(HENV_DATO_A_COL_NAME)
+                        .plusYears(1L)
+                        .isBefore(it.fieldAs<LocalDate>(FORSTE_SAMT_A_COL_NAME))
         }
         .map {
+            val henvendelsesDato = it.fieldAs<LocalDate>(HENV_DATO_A_COL_NAME)
+            val samtaleDato = it.fieldAs<LocalDate>(FORSTE_SAMT_A_COL_NAME)
+            val period = Period.between(henvendelsesDato, samtaleDato)
+
             createValidationReportEntry(
                 messageText = "Dato for primærklientens henvendelse '${it[HENV_DATO_A_COL_NAME]}' " +
                         "til familievernkontoret er mer enn 1 år før første behandlingssamtale " +
-                        "'${it[FORSTE_SAMT_A_COL_NAME]}'.",
+                        "'${it[FORSTE_SAMT_A_COL_NAME]}'." + period.toString(),
                 lineNumbers = listOf(it.lineNumber)
             ).copy(
-                caseworker = it[Familievern52aColumnNames.KONTOR_NR_A_COL_NAME],
-                journalId = it[Familievern52aColumnNames.JOURNAL_NR_A_COL_NAME]
+                caseworker = it[KONTOR_NR_A_COL_NAME],
+                journalId = it[JOURNAL_NR_A_COL_NAME]
             )
         }
         .ifEmpty { null }

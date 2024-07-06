dc8fc50fdd036a6418e7e8152a74b0dccd6232d7
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-07-06 15:01:59 +0200

    Revert FieldDefinitionExtensions.kt

--- kontroller/src/main/kotlin/no/ssb/kostra/program/extension/FieldDefinitionExtensions.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/program/extension/FieldDefinitionExtensions.kt
@@ -1,6 +1,5 @@
 package no.ssb.kostra.program.extension
 
-import no.ssb.kostra.program.DataType
 import no.ssb.kostra.program.FieldDefinition
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -10,10 +9,7 @@ fun FieldDefinition.codeIsMissing(code: String) = this.codeList.none { it.code =
 
 fun FieldDefinition.codeExists(code: String) = this.codeList.any { it.code == code }
 
-fun Collection<FieldDefinition>.byColumnName(columnName: String) =
-    this
-        .firstOrNull { it.name == columnName }
-        ?: FieldDefinition(name = "NOT FOUND", dataType = DataType.STRING_TYPE)
+fun Collection<FieldDefinition>.byColumnName(columnName: String) = this.first { it.name == columnName }
 
 fun List<FieldDefinition>.buildFieldDefinitions() =
     AtomicInteger(1)

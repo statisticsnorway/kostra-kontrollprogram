cf3da0df5fcc01eb40d8c5808a1a79d22081d4de
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-07-08 10:07:03 +0200

    Update Rule020AViktigsteKildeTilLivsOppholdKode3.kt

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/sosial/sosialhjelp/rule/Rule020AViktigsteKildeTilLivsOppholdKode3.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/sosial/sosialhjelp/rule/Rule020AViktigsteKildeTilLivsOppholdKode3.kt
@@ -3,7 +3,7 @@ package no.ssb.kostra.validation.rule.sosial.sosialhjelp.rule
 import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpColumnNames
 import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpColumnNames.TRYGDESIT_COL_NAME
 import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpColumnNames.VKLO_COL_NAME
-import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpFieldDefinitions.fieldDefinitions
+import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpFieldDefinitions
 import no.ssb.kostra.program.KostraRecord
 import no.ssb.kostra.program.extension.byColumnName
 import no.ssb.kostra.validation.report.Severity
@@ -18,13 +18,15 @@ class Rule020AViktigsteKildeTilLivsOppholdKode3 : AbstractNoArgsRule<List<Kostra
         .filterNot { it[VKLO_COL_NAME] == "3" }
         .filterNot { it[TRYGDESIT_COL_NAME] in validCodes }
         .map {
-            val vklo = fieldDefinitions
+            val vklo = SosialhjelpFieldDefinitions
+                .fieldDefinitions
                 .byColumnName(VKLO_COL_NAME)
                 .codeList
                 .firstOrNull { item -> item.code == it[VKLO_COL_NAME] }
                 ?.value
                 ?: "ukjent"
-            val codeList = fieldDefinitions
+            val codeList = SosialhjelpFieldDefinitions
+                .fieldDefinitions
                 .byColumnName(TRYGDESIT_COL_NAME)
                 .codeList
                 .filter { item -> item.code in validCodes }

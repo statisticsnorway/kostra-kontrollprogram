44f531dc790fa5c47a2de1e80b49e17f0719ba4b
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-09-02 11:06:17 +0200

    Update barnevern Individ.xsd (#683)
    * Barnevern
      * Update Individ.xsd
        * New codes where added earlier, but updating the restriction was forgotten.
        * Fix indentation
      * Update MelderTypeTest.kt

--- kostra-barnevern/src/test/kotlin/no/ssb/kostra/barnevern/xsd/MelderTypeTest.kt
+++ kostra-barnevern/src/test/kotlin/no/ssb/kostra/barnevern/xsd/MelderTypeTest.kt
@@ -45,7 +45,7 @@ class MelderTypeTest : BehaviorSpec({
                 "invalid Kode",
                 "<Melder Kode=\"42\"><Presisering>~Presisering~</Presisering></Melder>",
                 "cvc-enumeration-valid: Value '42' is not facet-valid with respect to enumeration " +
-                        "'[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]'. " +
+                        "'[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]'. " +
                         "It must be a value from the enumeration."
             ),
 

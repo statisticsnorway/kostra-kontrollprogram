d65c1da373e3059416583f7ceb3bb71b0ba11ca1
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-09-14 16:24:47 +0200

    Added frontend tests

--- web-frontend/src/features/mainForm/MainForm.spec.tsx
+++ web-frontend/src/features/mainForm/MainForm.spec.tsx
@@ -1,81 +1,162 @@
-import {beforeEach, describe, expect, it} from 'vitest'
-import {act, fireEvent, render, screen} from '@testing-library/react'
+import {beforeEach, describe, expect, it, Mock, vi} from 'vitest'
+import {act, fireEvent, render, screen, waitFor} from '@testing-library/react'
 import MainForm from "./MainForm"
+import KostraFormTypeVm from "../../kostratypes/kostraFormTypeVm"
+
+const formTypeOne = {
+    id: "~id~",
+    tittel: "~tittel1~",
+    labelOrgnr: null,
+    labelOrgnrVirksomhetene: null
+}
+const formTypeTwo = {
+    id: "~idWithCompanyId~",
+    tittel: "~tittel2~",
+    labelOrgnr: "Organisasjonsnummer",
+    labelOrgnrVirksomhetene: null
+}
+const formTypeThree = {
+    id: "~idWithSubCompanyId~",
+    tittel: "~tittel3~",
+    labelOrgnr: "Organisasjonsnummer for foretaket",
+    labelOrgnrVirksomhetene: "Organisasjonsnummer for virksomhetene"
+}
+
+const yearInTests = (new Date()).getFullYear()
+const yearInTestAsString = String(yearInTests)
+const mockFile = new File(["file content"], "some_file.dat", {
+    type: "text/plain"
+})
+
+const setupForSubmit = async (formType: KostraFormTypeVm = formTypeOne) => {
+    const fileInput = screen.getByLabelText("Datafil (.dat eller .xml)")
+    fireEvent.change(fileInput, {target: {files: [mockFile]}})
+
+    const skjemaSelect = screen.getByLabelText("Skjema")
+    fireEvent.change(skjemaSelect, {target: {value: formType.id}})
+
+    const yearSelect = screen.getByLabelText("Årgang")
+    fireEvent.change(yearSelect, {target: {value: yearInTestAsString}})
+
+    const regionInput = screen.getByLabelText("Regionsnummer")
+    fireEvent.change(regionInput, {target: {value: "123456"}})
+
+    let orgnrInput: HTMLInputElement, orgnrVirksomhetInput: HTMLInputElement
+
+    if (formType.labelOrgnr) {
+        orgnrInput = screen.getByLabelText(formType.labelOrgnr as string)
+        fireEvent.change(orgnrInput, {target: {value: "999999999"}})
+    }
+
+    if (formType.labelOrgnrVirksomhetene) {
+        orgnrVirksomhetInput = screen.getAllByPlaceholderText<HTMLInputElement>("9 siffer")[1]
+        fireEvent.change(orgnrVirksomhetInput, {target: {value: "888888888"}})
+    }
+
+    // @ts-ignore
+    await waitFor(() => {
+        // @ts-ignore
+        expect(fileInput.files[0].name).toBe(mockFile.name)
+        expect(skjemaSelect).toHaveValue(formType.id)
+        expect(yearSelect).toHaveValue(yearInTestAsString)
+        expect(regionInput).toHaveValue("123456")
+
+        if (formType.labelOrgnr) {
+            expect(orgnrInput).toHaveValue("999999999")
+        }
+
+        if (formType.labelOrgnrVirksomhetene) {
+            expect(orgnrVirksomhetInput).toHaveValue("888888888")
+        }
+    })
+}
 
 describe("MainForm", () => {
-    describe("Layout", () => {
-        it("displays displays 2 selects, 1 text input, 1 file input and 1 button", async () => {
-            await act(async () => render(<MainForm
-                formTypes={[]}
-                years={[(new Date()).getFullYear()]}
-                onSubmit={() => {
-                }}/>))
+    let formTypeSelect: HTMLSelectElement
+    let mockOnSubmit: Mock
 
+    beforeEach(async () => {
+        mockOnSubmit = vi.fn()
+
+        await act(() => render(<MainForm
+            formTypes={[formTypeOne, formTypeTwo, formTypeThree]}
+            years={[yearInTests]}
+            onSubmit={mockOnSubmit}/>))
+
+        formTypeSelect = screen.getByLabelText<HTMLSelectElement>("Skjema")
+    })
+
+    describe("Layout", () => {
+        it("displays 2 selects, 1 text input, 1 file input and 1 button initially", async () => {
             expect(screen.getByRole<HTMLOptionElement>("option", {name: "Velg skjematype"}).selected).toBeTruthy()
             expect(screen.getByRole<HTMLOptionElement>("option", {name: "Velg årgang"}).selected).toBeTruthy()
             expect(screen.getByLabelText<HTMLInputElement>("Regionsnummer").placeholder).toBe("6 siffer")
-            expect(screen.getByLabelText<HTMLInputElement>("Datafil (.dat eller .xml)")).toBeInTheDocument()
-            expect(screen.getByRole<HTMLButtonElement>("button", {name: "Kontroller fil"}).disabled).toBeTruthy()
+            expect(screen.getByLabelText("Datafil (.dat eller .xml)")).toBeInTheDocument()
+            expect(screen.getByRole("button", {name: "Kontroller fil"})).toBeDisabled()
         })
-    })
 
-    describe("Interactions", () => {
-        const formTypeOne = {
-            id: "~id~",
-            tittel: "~tittel1~",
-            labelOrgnr: null,
-            labelOrgnrVirksomhetene: null
-        }
-        const formTypeTwo = {
-            id: "~idWithCompanyId~",
-            tittel: "~tittel2~",
-            labelOrgnr: "Organisasjonsnummer",
-            labelOrgnrVirksomhetene: null
-        }
-        const formTypeThree = {
-            id: "~idWithSubCompanyId~",
-            tittel: "~tittel3~",
-            labelOrgnr: "Organisasjonsnummer for foretaket",
-            labelOrgnrVirksomhetene: "Organisasjonsnummer for virksomhetene"
-        }
+        it("displays validation errors initially", () => {
+            expect(screen.queryByText("Skjematype er påkrevet")).toBeInTheDocument()
+            expect(screen.queryByText("Årgang er påkrevet")).toBeInTheDocument()
+            expect(screen.queryByText("Region er påkrevet")).toBeInTheDocument()
+            expect(screen.queryByText("Vennligst velg fil")).toBeInTheDocument()
+        })
 
-        let formTypeSelect: HTMLSelectElement
+        it("hides validation errors when provided valid inputs", async () => {
+            await setupForSubmit()
 
-        beforeEach(() => {
-            render(<MainForm
-                formTypes={[formTypeOne, formTypeTwo, formTypeThree]}
-                years={[(new Date()).getFullYear()]}
-                onSubmit={() => {
-                }}/>)
+            expect(screen.queryByText("Skjematype er påkrevet")).not.toBeInTheDocument()
+            expect(screen.queryByText("Årgang er påkrevet")).not.toBeInTheDocument()
+            expect(screen.queryByText("Region er påkrevet")).not.toBeInTheDocument()
+            expect(screen.queryByText("Vennligst velg fil")).not.toBeInTheDocument()
+        })
 
-            formTypeSelect = screen.getByLabelText<HTMLSelectElement>("Skjema")
+        it("displays invalid value validation error for Regionsnummer when provided invalid value", async () => {
+            fireEvent.change(screen.getByLabelText("Regionsnummer"), {target: {value: "123"}})
+
+            await waitFor(() =>
+                expect(screen.queryByText("Region må bestå av 6 siffer")).toBeInTheDocument())
+        })
+
+        it("displays required validation error for Organisasjonsnummer when not provided", async () => {
+            fireEvent.change(formTypeSelect, {target: {value: formTypeTwo.id}})
+
+            await waitFor(() =>
+                expect(screen.queryByText("Organisasjonsnummer er påkrevet")).toBeInTheDocument())
+        })
+
+        it("displays invalid value validation error for Organisasjonsnummer when provided invalid value", async () => {
+            fireEvent.change(formTypeSelect, {target: {value: formTypeTwo.id}})
+            fireEvent.change(screen.getByLabelText(formTypeTwo.labelOrgnr), {target: {value: "123"}})
+
+            await waitFor(() =>
+                expect(screen.queryByText("Må starte med '8' eller '9' etterfulgt av 8 siffer")).toBeInTheDocument())
         })
 
         it("hides inputs for company-id and sub-company-id when a form type is selected", async () => {
-            await act(async () => {
-                fireEvent.change(formTypeSelect, {target: {value: formTypeOne.id}})
-            })
+            fireEvent.change(formTypeSelect, {target: {value: formTypeOne.id}})
 
             // verify that inputs are not in the document
-            expect(screen.queryByLabelText(formTypeTwo.labelOrgnr)).not.toBeInTheDocument()
-            expect(screen.queryByLabelText(formTypeThree.labelOrgnr)).not.toBeInTheDocument()
-            expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).not.toBeInTheDocument()
+            await waitFor(() => {
+                expect(screen.queryByText(formTypeTwo.labelOrgnr)).not.toBeInTheDocument()
+                expect(screen.queryByText(formTypeThree.labelOrgnr)).not.toBeInTheDocument()
+                expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).not.toBeInTheDocument()
+            })
         })
 
         it("displays input for company-id when form type is selected", async () => {
-
             // verify that input is not in the document
-            expect(screen.queryByLabelText(formTypeTwo.labelOrgnr)).not.toBeInTheDocument()
+            expect(screen.queryByText(formTypeTwo.labelOrgnr)).not.toBeInTheDocument()
 
-            await act(async () => {
-                fireEvent.change(formTypeSelect, {target: {value: formTypeTwo.id}})
+            fireEvent.change(formTypeSelect, {target: {value: formTypeTwo.id}})
+
+            await waitFor(() => {
+                // verify that input is in the document
+                expect(screen.queryByText(formTypeTwo.labelOrgnr)).toBeInTheDocument()
+
+                // ... and not the other one
+                expect(screen.queryByText(formTypeThree.labelOrgnr)).not.toBeInTheDocument()
             })
-
-            // verify that input is in the document
-            expect(screen.queryByLabelText(formTypeTwo.labelOrgnr)).toBeInTheDocument()
-
-            // ... and not the other one
-            expect(screen.queryByLabelText(formTypeThree.labelOrgnr)).not.toBeInTheDocument()
         })
 
         it("displays inputs for company-id and sub-company-id when form type is selected", async () => {
@@ -83,13 +164,75 @@ describe("MainForm", () => {
             expect(screen.queryByText(formTypeThree.labelOrgnr)).not.toBeInTheDocument
             expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).not.toBeInTheDocument
 
-            await act(async () => {
-                fireEvent.change(formTypeSelect, {target: {value: formTypeThree.id}})
-            })
+            fireEvent.change(formTypeSelect, {target: {value: formTypeThree.id}})
 
-            // verify that inputs are in the document
-            expect(screen.queryByLabelText(formTypeThree.labelOrgnr)).toBeInTheDocument()
-            expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).toBeInTheDocument()
+            await waitFor(() => {
+                // verify that inputs are in the document
+                expect(screen.queryByText(formTypeThree.labelOrgnr)).toBeInTheDocument()
+                expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).toBeInTheDocument()
+            })
+        })
+
+        it("enables the submit button when provided valid form", async () => {
+            const submitButton = screen.getByRole("button", {name: "Kontroller fil"})
+            expect(submitButton).toBeDisabled()
+
+            await setupForSubmit()
+
+            expect(submitButton).toBeEnabled()
+        })
+    })
+
+    describe("Interactions", () => {
+        const expectedBaseCallArgs = {
+            aar: yearInTests,
+            orgnrForetak: null,
+            orgnrVirksomhet: [],
+            region: "123456",
+            skjema: formTypeOne.id,
+            skjemaFil: [expect.objectContaining({
+                name: mockFile.name,
+                type: mockFile.type,
+            })]
+        }
+
+        const runSubmitTest = async (formType: KostraFormTypeVm, expectedCallArgs: object) => {
+            await setupForSubmit(formType)
+            const submitButton = screen.getByRole("button", {name: "Kontroller fil"})
+
+            fireEvent.click(submitButton)
+
+            await waitFor(() => {
+                expect(mockOnSubmit).toHaveBeenCalledTimes(1)
+                expect(mockOnSubmit).toBeCalledWith(expectedCallArgs)
+            })
+        }
+
+        it("calls onSubmit when submit button is clicked", async () => {
+            await runSubmitTest(formTypeOne, expectedBaseCallArgs)
+        })
+
+        it("calls onSubmit with orgnrForetak when submit button is clicked", async () => {
+            await runSubmitTest(
+                formTypeTwo,
+                {
+                    ...expectedBaseCallArgs,
+                    skjema: formTypeTwo.id,
+                    orgnrForetak: "999999999"
+                }
+            )
+        })
+
+        it("calls onSubmit with orgnrForetak and orgnrVirksomhet when submit button is clicked", async () => {
+            await runSubmitTest(
+                formTypeThree,
+                {
+                    ...expectedBaseCallArgs,
+                    skjema: formTypeThree.id,
+                    orgnrForetak: "999999999",
+                    orgnrVirksomhet: [{orgnr: "888888888"}]
+                }
+            )
         })
     })
 })

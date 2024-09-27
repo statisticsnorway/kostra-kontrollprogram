18999a972ba446179f2df004fe138e83beda3459
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-09-27 16:30:50 +0200

    Fixed scss warning

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -13,5 +13,12 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler"
+      }
+    }
   }
 })

a4a6f724005d5cb522235e9c7e399b8ded141cac
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-11-05 14:19:22 +0100

    Bugfixed famvern mapping (#751)
    * Bugfixed famvern mapping
    
    * Update integration test

--- kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/famvern/FamilievernConstantsTest.kt
+++ kontroller/src/test/kotlin/no/ssb/kostra/validation/rule/famvern/FamilievernConstantsTest.kt
@@ -5,45 +5,55 @@ import io.kotest.matchers.collections.shouldContain
 import no.ssb.kostra.area.famvern.FamilievernConstants
 import no.ssb.kostra.program.FileLoader
 
-class FamilievernConstantsTest : BehaviorSpec({
-    Given("a file name of a mapping file") {
-        val fileName = "mapping_familievern_region_fylke_kontor_2024.yaml"
+class FamilievernConstantsTest :
+    BehaviorSpec({
+        Given("a file name of a mapping file") {
+            val fileName = "mapping_familievern_region_fylke_kontor_2024.yaml"
 
-        When("opening the mapping file") {
+            When("opening the mapping file") {
 
-            val result = FileLoader
-                .getResource<FamilievernConstants.MappingDescription>(fileName)
+                val result =
+                    FileLoader
+                        .getResource<FamilievernConstants.MappingDescription>(fileName)
 
-            Then("result is not empty and should contain data") {
-                result.title.isNotEmpty()
-                result.year != 0
-                result.description.isNotEmpty()
-                result.regions.isNotEmpty()
-                result.regions.first().counties.isNotEmpty()
-                result.regions.first().counties.first().offices.isNotEmpty()
+                Then("result is not empty and should contain data") {
+                    result.title.isNotEmpty()
+                    result.year != 0
+                    result.description.isNotEmpty()
+                    result.regions.isNotEmpty()
+                    result.regions
+                        .first()
+                        .counties
+                        .isNotEmpty()
+                    result.regions
+                        .first()
+                        .counties
+                        .first()
+                        .offices
+                        .isNotEmpty()
+                }
             }
         }
-    }
 
-    Given("FamilievernConstants.kontorFylkeRegionMappingList") {
-        val sut = FamilievernConstants.kontorFylkeRegionMappingList
+        Given("FamilievernConstants.kontorFylkeRegionMappingList") {
+            val sut = FamilievernConstants.kontorFylkeRegionMappingList
 
-        Then("kontorFylkeRegionMappingList should not be empty and should contain data") {
-            sut.isNotEmpty()
-            sut.shouldContain(
-                FamilievernConstants.KontorFylkeRegionMapping(
-                    region = "667200",
-                    fylke = "03",
-                    kontor = "030",
-                ),
-            )
-            sut.shouldContain(
-                FamilievernConstants.KontorFylkeRegionMapping(
-                    region = "667600",
-                    fylke = "56",
-                    kontor = "205",
-                ),
-            )
+            Then("kontorFylkeRegionMappingList should not be empty and should contain data") {
+                sut.isNotEmpty()
+                sut.shouldContain(
+                    FamilievernConstants.KontorFylkeRegionMapping(
+                        region = "667600",
+                        fylke = "03",
+                        kontor = "030",
+                    ),
+                )
+                sut.shouldContain(
+                    FamilievernConstants.KontorFylkeRegionMapping(
+                        region = "667200",
+                        fylke = "56",
+                        kontor = "205",
+                    ),
+                )
+            }
         }
-    }
-})
+    })
--- web/src/test/kotlin/no/ssb/kostra/web/service/ControlRunnerTest.kt
+++ web/src/test/kotlin/no/ssb/kostra/web/service/ControlRunnerTest.kt
@@ -11,44 +11,47 @@ import no.ssb.kostra.web.viewmodel.KostraFormVm
 
 @MicronautTest
 class ControlRunnerTest(
-    sut: ControlRunner
+    sut: ControlRunner,
 ) : BehaviorSpec({
-    Given("request with non-fatal error") {
-        val kostraForm = KostraFormVm(
-            aar = 2022,
-            skjema = "52AF",
-            region = "667200",
-            filnavn = "fil.data",
-            orgnrVirksomhet = emptyList()
-        )
-
-        When("validateDataFile with valid content") {
-            val errorReport = sut.runControls(
-                kostraForm = kostraForm,
-                inputStream = PLAIN_TEXT.toByteArray().inputStream()
-            )
-
-            Then("errorReportVm should be as expected") {
-                errorReport.severity shouldBe Severity.OK
-                errorReport.antallKontroller.shouldBeGreaterThan(50)
-                errorReport.feil.shouldBeEmpty()
-            }
-        }
-
-        When("validateDataFile without content") {
-            shouldNotThrowAny {
-                sut.runControls(
-                    kostraForm = kostraForm,
-                    inputStream = "".byteInputStream()
+        Given("request with non-fatal error") {
+            val kostraForm =
+                KostraFormVm(
+                    aar = 2022,
+                    skjema = "52AF",
+                    region = "667600",
+                    filnavn = "fil.data",
+                    orgnrVirksomhet = emptyList(),
                 )
+
+            When("validateDataFile with valid content") {
+                val errorReport =
+                    sut.runControls(
+                        kostraForm = kostraForm,
+                        inputStream = PLAIN_TEXT.toByteArray().inputStream(),
+                    )
+
+                Then("errorReportVm should be as expected") {
+                    errorReport.severity shouldBe Severity.OK
+                    errorReport.antallKontroller.shouldBeGreaterThan(50)
+                    errorReport.feil.shouldBeEmpty()
+                }
+            }
+
+            When("validateDataFile without content") {
+                shouldNotThrowAny {
+                    sut.runControls(
+                        kostraForm = kostraForm,
+                        inputStream = "".byteInputStream(),
+                    )
+                }
             }
         }
-    }
-}) {
+    }) {
     companion object {
-        private val PLAIN_TEXT = """
-            6672000170201001000101202011119761 1113  1       0302202021                 12122222220302              003000003005      00300521          10120052020
-            6672000170201024000101202011119761 1113  1       0302202021                 12 22222220302              003000003005      00300521          10120052020
+        private val PLAIN_TEXT =
+            """
+            6676000170201001000101202011119761 1113  1       0302202021                 12122222220302              003000003005      00300521          10120052020
+            6676000170201024000101202011119761 1113  1       0302202021                 12 22222220302              003000003005      00300521          10120052020
             """.trimIndent()
     }
 }

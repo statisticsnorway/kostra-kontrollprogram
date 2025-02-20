88dec279435471a6f7f856ae96f67561c7adba2c
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-02-20 11:29:28 +0100

    Remove businesses in frontend

--- web-frontend/src/features/mainForm/MainForm.spec.tsx
+++ web-frontend/src/features/mainForm/MainForm.spec.tsx
@@ -15,12 +15,6 @@ const formTypeTwo = {
     labelOrgnr: "Organisasjonsnummer",
     labelOrgnrVirksomhetene: null
 }
-const formTypeThree = {
-    id: "~idWithSubCompanyId~",
-    tittel: "~tittel3~",
-    labelOrgnr: "Organisasjonsnummer for foretaket",
-    labelOrgnrVirksomhetene: "Organisasjonsnummer for virksomhetene"
-}
 
 const yearInTests = (new Date()).getFullYear()
 const yearInTestAsString = String(yearInTests)
@@ -44,13 +38,8 @@ const setupForSubmit = async (formType: KostraFormTypeVm = formTypeOne) => {
     let orgnrInput: HTMLInputElement, orgnrVirksomhetInput: HTMLInputElement
 
     if (formType.labelOrgnr) {
-        orgnrInput = screen.getByLabelText(formType.labelOrgnr as string)
+        orgnrInput = screen.getByLabelText(formType.labelOrgnr)
         fireEvent.change(orgnrInput, {target: {value: "999999999"}})
-    }
-
-    if (formType.labelOrgnrVirksomhetene) {
-        orgnrVirksomhetInput = screen.getByTestId("orgnrVirksomhet.0.orgnr")
-        fireEvent.change(orgnrVirksomhetInput, {target: {value: "888888888"}})
     }
 
     await waitFor(() => {
@@ -63,10 +52,6 @@ const setupForSubmit = async (formType: KostraFormTypeVm = formTypeOne) => {
         if (formType.labelOrgnr) {
             expect(orgnrInput).toHaveValue("999999999")
         }
-
-        if (formType.labelOrgnrVirksomhetene) {
-            expect(orgnrVirksomhetInput).toHaveValue("888888888")
-        }
     })
 }
 
@@ -78,7 +63,7 @@ describe("MainForm", () => {
         mockOnSubmit = vi.fn()
 
         await act(() => render(<MainForm
-            formTypes={[formTypeOne, formTypeTwo, formTypeThree]}
+            formTypes={[formTypeOne, formTypeTwo]}
             years={[yearInTests]}
             onSubmit={mockOnSubmit}/>))
 
@@ -132,40 +117,12 @@ describe("MainForm", () => {
                 expect(screen.queryByText("Må starte med '8' eller '9' etterfulgt av 8 siffer")).toBeInTheDocument())
         })
 
-        it("displays validation error for Organisasjonsnummer for virksomhetene when provided invalid value", async () => {
-            fireEvent.change(formTypeSelect, {target: {value: formTypeThree.id}})
-
-            const orgnrVirksomhet = await waitFor(() =>
-                screen.getByTestId("orgnrVirksomhet.0.orgnr"))
-
-            fireEvent.change(orgnrVirksomhet as HTMLInputElement, {target: {value: "123"}})
-
-            await waitFor(() =>
-                expect(orgnrVirksomhet?.className).toBe("form-control is-invalid"))
-        })
-
-        it("displays plus button when valid Organisasjonsnummer for virksomhetene", async () => {
-            await setupForSubmit(formTypeThree)
-            expect(screen.getByRole("button", {description: "Legg til virksomhetsnummer"})).toBeInTheDocument()
-        })
-
-        it("displays minus button when second Organisasjonsnummer for virksomhetene exists", async () => {
-            await setupForSubmit(formTypeThree)
-
-            fireEvent.click(screen.getByRole("button", {description: "Legg til virksomhetsnummer"}))
-
-            await waitFor(() =>
-                expect(screen.getByRole("button", {description: "Fjern virksomhetsnummer"})).toBeInTheDocument())
-        })
-
         it("hides inputs for company-id and sub-company-id when a form type is selected", async () => {
             fireEvent.change(formTypeSelect, {target: {value: formTypeOne.id}})
 
             // verify that inputs are not in the document
             await waitFor(() => {
                 expect(screen.queryByText(formTypeTwo.labelOrgnr)).not.toBeInTheDocument()
-                expect(screen.queryByText(formTypeThree.labelOrgnr)).not.toBeInTheDocument()
-                expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).not.toBeInTheDocument()
             })
         })
 
@@ -178,23 +135,6 @@ describe("MainForm", () => {
             await waitFor(() => {
                 // verify that input is in the document
                 expect(screen.queryByText(formTypeTwo.labelOrgnr)).toBeInTheDocument()
-
-                // ... and not the other one
-                expect(screen.queryByText(formTypeThree.labelOrgnr)).not.toBeInTheDocument()
-            })
-        })
-
-        it("displays inputs for company-id and sub-company-id when form type is selected", async () => {
-            // verify that inputs are not in the document
-            expect(screen.queryByText(formTypeThree.labelOrgnr)).not.toBeInTheDocument
-            expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).not.toBeInTheDocument
-
-            fireEvent.change(formTypeSelect, {target: {value: formTypeThree.id}})
-
-            await waitFor(() => {
-                // verify that inputs are in the document
-                expect(screen.queryByText(formTypeThree.labelOrgnr)).toBeInTheDocument()
-                expect(screen.queryByText(formTypeThree.labelOrgnrVirksomhetene)).toBeInTheDocument()
             })
         })
 
@@ -209,28 +149,9 @@ describe("MainForm", () => {
     })
 
     describe("Interactions", () => {
-        it("removes second Organisasjonsnummer for virksomhetene when minus button is clicked", async () => {
-            await setupForSubmit(formTypeThree)
-
-            const plusButton =
-                screen.getByRole("button", {description: "Legg til virksomhetsnummer"})
-
-            fireEvent.click(plusButton)
-
-            const minusButton =
-                await waitFor(() => screen.getByRole("button", {description: "Fjern virksomhetsnummer"}))
-
-            fireEvent.click(minusButton)
-
-            await waitFor(() =>
-                expect(minusButton).not.toBeInTheDocument())
-        })
-
-
         const expectedBaseCallArgs = {
             aar: yearInTests,
             orgnrForetak: null,
-            orgnrVirksomhet: [],
             region: "123456",
             skjema: formTypeOne.id,
             skjemaFil: [expect.objectContaining({
@@ -262,18 +183,6 @@ describe("MainForm", () => {
                     ...expectedBaseCallArgs,
                     skjema: formTypeTwo.id,
                     orgnrForetak: "999999999"
-                }
-            )
-        })
-
-        it("calls onSubmit with orgnrForetak and orgnrVirksomhet when submit button is clicked", async () => {
-            await runSubmitTest(
-                formTypeThree,
-                {
-                    ...expectedBaseCallArgs,
-                    skjema: formTypeThree.id,
-                    orgnrForetak: "999999999",
-                    orgnrVirksomhet: [{orgnr: "888888888"}]
                 }
             )
         })

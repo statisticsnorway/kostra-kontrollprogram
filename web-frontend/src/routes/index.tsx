874d12a5a536716aa4f62b1aa8983d1aae609a1f
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-02-27 22:12:19 +0100

    Race-condition fix

--- web-frontend/src/routes/index.tsx
+++ web-frontend/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import {kontrollerSkjemaAsync} from "../api/apiCalls"
 import KostraFormVm from "../kostratypes/kostraFormVm"
-import {useCallback, useState} from "react"
+import {useCallback, useEffect, useState} from "react"
 import FileReportVm from "../kostratypes/fileReportVm"
 import {useNavigate} from "react-router-dom"
 import MainForm from "../features/mainForm/MainForm"
@@ -10,8 +10,17 @@ const Index = ({uiData, onAddFileReport}: {
     uiData: uiDataVm,
     onAddFileReport: (fileReport: NonNullable<FileReportVm>) => void
 }) => {
-    const [isPostError, setIsPostError] = useState<boolean>(false)
     const navigate = useNavigate()
+
+    const [isPostError, setIsPostError] = useState<boolean>(false)
+    const [doNavigateToReport, setDoNavigateToReport] = useState<boolean>(false)
+
+    useEffect(() => {
+        if (doNavigateToReport) {
+            navigate("/file-reports/0")
+            setDoNavigateToReport(false)
+        }
+    }, [doNavigateToReport, navigate])
 
     // Submits form to backend and stores returned report at start of fileReports array state.
     const onSubmit = useCallback((form: KostraFormVm) =>
@@ -19,12 +28,12 @@ const Index = ({uiData, onAddFileReport}: {
             .then(fileReport => {
                 setIsPostError(false)
                 onAddFileReport(fileReport)
-                navigate("/file-reports/0") // new reports are added to beginning of list
+                setDoNavigateToReport(true)
             })
             .catch((error) => {
                 if (error.response) console.log(error.response)
                 setIsPostError(true)
-            }), [onAddFileReport, navigate])
+            }), [onAddFileReport])
 
     return <>
         {isPostError && <span className="text-danger">Feil ved kontroll av fil</span>}

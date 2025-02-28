e6a62b8bb8835c3253024207402210a56ebf3e68
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-02-28 14:31:46 +0100

    Revert sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ subprojects {
     if (name != "kostra-kontrollprogram-web-frontend") {
         apply(plugin = "kotlin")
         apply(plugin = "jacoco")
-        apply(plugin = "sonarcube")
 
         kotlin { jvmToolchain(21) }
         repositories { mavenCentral() }

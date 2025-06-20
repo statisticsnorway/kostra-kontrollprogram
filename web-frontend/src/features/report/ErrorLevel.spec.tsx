d971721d29a1f667a71e9f7e8ce9079c677d1c26
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-06-20 20:50:44 +0200

    Debug frontend (#1045)
    * Create 52AF_ok.dat
    
    * Update Errorlevel to handle all severities explicitly
    
    * fix typo

--- web-frontend/src/features/report/ErrorLevel.spec.tsx
+++ web-frontend/src/features/report/ErrorLevel.spec.tsx
@@ -5,22 +5,22 @@ import KostraSeverity from "../../kostratypes/kostraSeverity"
 
 describe("ErrorLevel", () => {
     describe("Layout", () => {
-        it("no error, expect text 'OK'", () => {
+        it("ok, expect text 'OK'", () => {
             render(<ErrorLevel level={KostraSeverity.OK}/>)
             expect(screen.queryByText("OK")).toBeInTheDocument()
         })
-        it("no error, expect cssClass 'text-success'", () => {
+        it("ok, expect cssClass 'text-success'", () => {
             render(<ErrorLevel level={KostraSeverity.OK}/>)
             expect(screen.getByText("OK").classList.contains("text-success")).toBeTruthy()
         })
 
-        it("warning, expect text 'Info'", () => {
+        it("info, expect text 'Info'", () => {
             render(<ErrorLevel level={KostraSeverity.INFO}/>)
             expect(screen.queryByText("Info")).toBeInTheDocument()
         })
-        it("warning, expect cssClass 'text-success'", () => {
+        it("info, expect cssClass 'text-info'", () => {
             render(<ErrorLevel level={KostraSeverity.INFO}/>)
-            expect(screen.getByText("Info").classList.contains("text-success")).toBeTruthy()
+            expect(screen.getByText("Info").classList.contains("text-info")).toBeTruthy()
         })
 
         it("warning, expect text 'Advarsel'", () => {
@@ -32,20 +32,20 @@ describe("ErrorLevel", () => {
             expect(screen.getByText("Advarsel").classList.contains("text-warning")).toBeTruthy()
         })
 
-        it("critical error, expect text 'Kritisk'", () => {
+        it("error, expect text 'Feil'", () => {
             render(<ErrorLevel level={KostraSeverity.ERROR}/>)
-            expect(screen.queryByText("Kritisk")).toBeInTheDocument()
+            expect(screen.queryByText("Feil")).toBeInTheDocument()
         })
-        it("critical error, expect cssClass 'text-danger'", () => {
+        it("error, expect cssClass 'text-danger'", () => {
             render(<ErrorLevel level={KostraSeverity.ERROR}/>)
-            expect(screen.getByText("Kritisk").classList.contains("text-danger")).toBeTruthy()
+            expect(screen.getByText("Feil").classList.contains("text-danger")).toBeTruthy()
         })
 
         it("fatal error, expect text 'Kritisk'", () => {
             render(<ErrorLevel level={KostraSeverity.FATAL}/>)
             expect(screen.queryByText("Kritisk")).toBeInTheDocument()
         })
-        it("critical error, expect cssClass 'text-danger'", () => {
+        it("fatal error, expect cssClass 'text-danger'", () => {
             render(<ErrorLevel level={KostraSeverity.FATAL}/>)
             expect(screen.getByText("Kritisk").classList.contains("text-danger")).toBeTruthy()
         })
--- web-frontend/src/features/report/ErrorLevel.tsx
+++ web-frontend/src/features/report/ErrorLevel.tsx
@@ -2,13 +2,17 @@ import KostraSeverity from "../../kostratypes/kostraSeverity"
 
 const getClassName = (level: NonNullable<KostraSeverity>) => {
     switch (level) {
-        case KostraSeverity.INFO:
         case KostraSeverity.OK:
             return "text-success"
+        case KostraSeverity.INFO:
+            return "text-info"
         case KostraSeverity.WARNING:
             return "text-warning"
-        default:
+        case KostraSeverity.ERROR:
+        case KostraSeverity.FATAL:
             return "text-danger"
+        default:
+            return "text-muted"
     }
 }
 
@@ -20,8 +24,12 @@ const getErrorText = (level: NonNullable<KostraSeverity>) => {
             return "Info"
         case KostraSeverity.WARNING:
             return "Advarsel"
-        default:
+        case KostraSeverity.ERROR:
+            return "Feil"
+        case KostraSeverity.FATAL:
             return "Kritisk"
+        default:
+            return "Ukjent"
     }
 }
 

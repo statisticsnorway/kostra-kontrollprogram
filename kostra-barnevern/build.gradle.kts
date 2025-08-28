c0c0cd35ade9d60545ffd9cf2f6f41c07b42a4ad
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-08-28 08:30:05 +0200

    Switch off publishing of kostra-barnevern again

--- kostra-barnevern/build.gradle.kts
+++ kostra-barnevern/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     alias(libs.plugins.spring.dependency.management)
-    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.4" apply false
-    `maven-publish`
+//    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.4" apply false
+//    `maven-publish`
 }
 
- if (!project.version.toString().contains("SNAPSHOT")) {
-    apply(plugin = "com.google.cloud.artifactregistry.gradle-plugin")
- }
+//if (!project.version.toString().contains("SNAPSHOT")) {
+//    apply(plugin = "com.google.cloud.artifactregistry.gradle-plugin")
+//}
 
 dependencyManagement {
     dependencies {
@@ -25,17 +25,17 @@ dependencies {
     implementation(libs.woodstox.core)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            groupId = "no.ssb.kostra"
-            artifactId = "kostra-barnevern"
-            version = System.getenv("PROJECT_VERSION") ?: "LOCAL-SNAPSHOT"
-
-            from(components["java"])
-        }
-    }
-    repositories {
-        maven("artifactregistry://europe-north1-maven.pkg.dev/${System.getenv("GAR_PROJECT_ID")}/kostra-maven")
-    }
-}
+//publishing {
+//    publications {
+//        create<MavenPublication>("mavenJava") {
+//            groupId = "no.ssb.kostra"
+//            artifactId = "kostra-barnevern"
+//            version = System.getenv("PROJECT_VERSION") ?: "LOCAL-SNAPSHOT"
+//
+//            from(components["java"])
+//        }
+//    }
+//    repositories {
+//        maven("artifactregistry://europe-north1-maven.pkg.dev/${System.getenv("GAR_PROJECT_ID")}/kostra-maven")
+//    }
+//}

4f690dcacc49091e243444e80910e279897ba738
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2025-11-04 17:37:07 +0100

    Utvider konsoll-programmet til å kunne hente ut rapport i strukturert JSON-format, og å kunne definere charset.

--- konsoll/src/main/kotlin/no/ssb/kostra/program/KostraKontrollprogramCommand.kt
+++ konsoll/src/main/kotlin/no/ssb/kostra/program/KostraKontrollprogramCommand.kt
@@ -1,5 +1,6 @@
 package no.ssb.kostra.program
 
+import no.ssb.kostra.validation.report.StructuredValidationReport
 import no.ssb.kostra.validation.report.ValidationReport
 import picocli.CommandLine
 import picocli.CommandLine.Command
@@ -41,6 +42,12 @@ class KostraKontrollprogramCommand : Callable<Int> {
     @Option(names = ["-a", "--attachment"], description = ["..."])
     private var hasAttachment: String = "1"
 
+    @Option(names = ["-f", "--output-format"], defaultValue = "HTML", description = ["..."])
+    private var outputFormat: String = "HTML"
+
+    @Option(names = ["-c", "--output-charset"], description = ["..."])
+    private var outputCharset: String = "ISO-8859-1"
+
     @Option(names = ["-e", "--external-process"], description = ["..."])
     private var isRunAsExternalProcess: Boolean = false
 
@@ -53,7 +60,16 @@ class KostraKontrollprogramCommand : Callable<Int> {
     override fun call(): Int {
         if (verbose) dumpParamsToOutput()
 
-        return ControlDispatcher.validate(
+        when (outputCharset) {
+            "ISO-8859-1", "LATIN1" -> outputCharset = StandardCharsets.ISO_8859_1.name()
+            "UTF-8", "UTF8" -> outputCharset = StandardCharsets.UTF_8.name()
+            else -> {
+                System.err.println("Ukjent tegnsett for output: $outputCharset. Bruk ISO-8859-1 eller UTF-8.")
+                return -1
+            }
+        }
+
+        ControlDispatcher.validate(
             KotlinArguments(
                 skjema = schema,
                 aargang = year,
@@ -62,19 +78,32 @@ class KostraKontrollprogramCommand : Callable<Int> {
                 navn = name,
                 orgnr = companyId,
                 harVedlegg = (hasAttachment == "1"),
+                outputFormat = outputFormat,
                 isRunAsExternalProcess = isRunAsExternalProcess,
                 inputFileContent =
-                if (schema.isNotBlank() && hasAttachment == "1")
-                    System.`in`.bufferedReader().use(BufferedReader::readText)
-                else
-                    BLANK_CHAR
+                    if (schema.isNotBlank() && hasAttachment == "1")
+                        System.`in`.bufferedReader().use(BufferedReader::readText)
+                    else
+                        BLANK_CHAR
             )
         ).let { validationReportArguments ->
-            PrintStream(System.out, true, StandardCharsets.ISO_8859_1).use { printStream ->
-                printStream.print(ValidationReport(validationReportArguments))
+            if (outputFormat == "HTML") {
+                PrintStream(System.out, true, outputCharset).use { printStream ->
+                    printStream.print(ValidationReport(validationReportArguments))
+                }
+                return validationReportArguments.validationResult.severity.info.returnCode
+            } else if (outputFormat == "JSON") {
+                PrintStream(System.out, true, outputCharset).use { printStream ->
+                    printStream.print(StructuredValidationReport(validationReportArguments))
+                }
+                return validationReportArguments.validationResult.severity.info.returnCode
+
+            } else {
+                System.err.println("Ukjent output format: $outputFormat. Bruk HTML eller JSON.")
+                -1
             }
-            validationReportArguments.validationResult.severity.info.returnCode
         }
+        return 0
     }
 
     private fun dumpParamsToOutput() {

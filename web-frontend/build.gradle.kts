bf92f706dcabd72974d3244928cc1e0245fb59a8
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-12-17 14:15:30 +0100

    Revert Siouan plugin to v9.0.0

--- web-frontend/build.gradle.kts
+++ web-frontend/build.gradle.kts
@@ -1,28 +1,9 @@
-import org.siouan.frontendgradleplugin.infrastructure.gradle.RunNodeTaskType
-import org.siouan.frontendgradleplugin.infrastructure.gradle.RunNpmTaskType
-
 plugins {
-    id("org.siouan.frontend-jdk21") version "9.1.0"
+    id("org.siouan.frontend-jdk21") version "9.0.0"
 }
 
 frontend {
-    nodeDistributionProvided.set(true)
-    nodeDistributionUrlRoot.set("https://nodejs.org")
-    nodeVersion.set("24.12.0")
+    nodeVersion.set("22.14.0")
     assembleScript.set("run build")
     checkScript.set("run check")
-}
-
-tasks.register<RunNodeTaskType>("nodeVersion") {
-    description = "Triggers install Node for this project."
-    group = "node.js"
-    dependsOn("installNode")
-    args.set("-v")
-}
-
-tasks.register<RunNpmTaskType>("npmVersion") {
-    description = "Triggers install npm for this project."
-    group = "npm"
-    dependsOn("installPackageManager")
-    args.set("-v")
 }

cea86892b299bd52228b93c8b22420e54fcde93b
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-12-17 15:02:12 +0100

    Update build.gradle.kts

--- web-frontend/build.gradle.kts
+++ web-frontend/build.gradle.kts
@@ -3,7 +3,17 @@ plugins {
 }
 
 frontend {
+    nodeDistributionProvided.set(false)
+    nodeDistributionUrlRoot.set("https://nodejs.org/dist/")
     nodeVersion.set("24.12.0")
+
+    installScript.set("run install")
     assembleScript.set("run build")
     checkScript.set("run check")
+
+    nodeInstallDirectory.set(project.layout.projectDirectory.dir("node"))
+    corepackVersion.set("latest")
+
+    packageJsonDirectory.set(project.layout.projectDirectory)
+    cacheDirectory.set(project.layout.projectDirectory.dir(".frontend-gradle-plugin"))
 }

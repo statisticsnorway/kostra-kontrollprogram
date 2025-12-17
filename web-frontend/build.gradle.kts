27b96e71174449af33336db76f071dd87e8e5190
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-12-17 14:03:01 +0100

    Update build.gradle.kts

--- web-frontend/build.gradle.kts
+++ web-frontend/build.gradle.kts
@@ -1,9 +1,32 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
+import org.siouan.frontendgradleplugin.infrastructure.gradle.RunNodeTaskType
+import org.siouan.frontendgradleplugin.infrastructure.gradle.RunNpmTaskType
+
 plugins {
-    alias(libs.plugins.siouan.frontend.jdk21)
+    id("org.siouan.frontend-jdk21") version "9.1.0"
 }
 
+//val preinstalledNodeDistributionDirectory = file(<path-to-nodejs-install-directory>);
+val preinstalledNodeDistributionDirectory = null as File?;
+
 frontend {
+    nodeDistributionProvided.set(true)
+    nodeInstallDirectory.set(preinstalledNodeDistributionDirectory)
     nodeVersion.set("24.12.0")
     assembleScript.set("run build")
     checkScript.set("run check")
+}
+
+tasks.register<RunNodeTaskType>("nodeVersion") {
+    description = "Triggers install Node for this project."
+    group = "node.js"
+    dependsOn("installNode")
+    args.set("-v")
+}
+
+tasks.register<RunNpmTaskType>("npmVersion") {
+    description = "Triggers install npm for this project."
+    group = "npm"
+    dependsOn("installPackageManager")
+    args.set("-v")
 }

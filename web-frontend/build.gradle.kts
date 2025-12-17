70678b7a4ba850120f64999cecb50e71bde3e041
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-12-17 15:08:07 +0100

    Revert last commit

--- web-frontend/build.gradle.kts
+++ web-frontend/build.gradle.kts
@@ -3,17 +3,7 @@ plugins {
 }
 
 frontend {
-    nodeDistributionProvided.set(false)
-    nodeDistributionUrlRoot.set("https://nodejs.org/dist/")
     nodeVersion.set("24.12.0")
-
-    installScript.set("run install")
     assembleScript.set("run build")
     checkScript.set("run check")
-
-    nodeInstallDirectory.set(project.layout.projectDirectory.dir("node"))
-    corepackVersion.set("latest")
-
-    packageJsonDirectory.set(project.layout.projectDirectory)
-    cacheDirectory.set(project.layout.projectDirectory.dir(".frontend-gradle-plugin"))
 }

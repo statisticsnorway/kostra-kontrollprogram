2c1bed9777eef4af13457a358286fbe26408f199
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2025-12-17 14:25:09 +0100

    Force running installNode task when building

--- web-frontend/build.gradle.kts
+++ web-frontend/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("org.siouan.frontend-jdk21") version "9.0.0"
+    id("org.siouan.frontend-jdk21") version "10.0.0"
 }
 
 frontend {
-    nodeVersion.set("22.14.0")
+    nodeVersion.set("24.12.0")
     assembleScript.set("run build")
     checkScript.set("run check")
 }

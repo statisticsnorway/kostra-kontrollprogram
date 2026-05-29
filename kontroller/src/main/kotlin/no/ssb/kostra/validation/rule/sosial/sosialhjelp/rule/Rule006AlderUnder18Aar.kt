7b0ac596b6e008e4bf1628638a5d1def0f7dea5a
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2026-05-29 14:18:37 +0200

    Update Rule006AlderUnder18Aar.kt

--- kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/sosial/sosialhjelp/rule/Rule006AlderUnder18Aar.kt
+++ kontroller/src/main/kotlin/no/ssb/kostra/validation/rule/sosial/sosialhjelp/rule/Rule006AlderUnder18Aar.kt
@@ -1,6 +1,7 @@
 package no.ssb.kostra.validation.rule.sosial.sosialhjelp.rule
 
 import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpColumnNames
+import no.ssb.kostra.area.sosial.sosialhjelp.SosialhjelpColumnNames.PERSON_FODSELSNR_COL_NAME
 import no.ssb.kostra.program.KostraRecord
 import no.ssb.kostra.program.KotlinArguments
 import no.ssb.kostra.program.extension.ageInYears
@@ -12,19 +13,23 @@ class Rule006AlderUnder18Aar : AbstractRule<List<KostraRecord>>(
     SosialRuleId.ALDER_UNDER_18_AAR_06.title,
     Severity.WARNING
 ) {
-    override fun validate(context: List<KostraRecord>, arguments: KotlinArguments) = context
-        .filterNot {
-            it[SosialhjelpColumnNames.PERSON_FODSELSNR_COL_NAME].ageInYears(arguments.aargang.toInt())
-                ?.let { age -> 18 <= age } ?: true
-        }.map {
-            val alder = it[SosialhjelpColumnNames.PERSON_FODSELSNR_COL_NAME].ageInYears(arguments.aargang.toInt())
-
+    override fun validate(
+        context: List<KostraRecord>,
+        arguments: KotlinArguments
+    ) = context
+        .map { record ->
+            (record to (record[PERSON_FODSELSNR_COL_NAME].ageInYears(
+                arguments.aargang.toInt()
+            ) ?: -1))
+        }
+        .filter { (_, age) -> age < 18 }
+        .map { (record, age) ->
             createValidationReportEntry(
-                messageText = "Deltakeren ($alder år) er under 18 år.",
-                lineNumbers = listOf(it.lineNumber)
+                messageText = "Deltakeren ($age år) er under 18 år.",
+                lineNumbers = listOf(record.lineNumber)
             ).copy(
-                caseworker = it[SosialhjelpColumnNames.SAKSBEHANDLER_COL_NAME],
-                journalId = it[SosialhjelpColumnNames.PERSON_JOURNALNR_COL_NAME],
+                caseworker = record[SosialhjelpColumnNames.SAKSBEHANDLER_COL_NAME],
+                journalId = record[SosialhjelpColumnNames.PERSON_JOURNALNR_COL_NAME],
             )
         }.ifEmpty { null }
 }
